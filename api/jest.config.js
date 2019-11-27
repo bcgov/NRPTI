d458d86a79532b7f3a9a8f8b3eaa87b118ac0e4d
Mark Lisé <marklise@users.noreply.github.com>
2019-11-26 16:52:58 -0800

    Updating unit tests, locations, re-organising, code coverage updates, fixing harness. (#36)

--- api/jest.config.js
+++
@@ -1,9 +0,0 @@
-const { defaults } = require('jest-config'); // https://jestjs.io/docs/en/configuration
-
-module.exports = {
-  ...defaults,
-  collectCoverage: true,
-  collectCoverageFrom: ['./api/**/*.js', '!./api/**/*.test.js', '!./api/test/**'],
-  coverageDirectory: './coverage',
-  testEnvironment: 'node'
-};
