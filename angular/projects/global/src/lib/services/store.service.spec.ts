b849f9399b6eb6640ce3245078e8d9c83cfa89d5
Mark Lisé <marklise@users.noreply.github.com>
2019-11-29 12:55:34 -0800

    Update jenkins,env,tests (#44)

--- angular/projects/global/src/lib/services/store.service.spec.ts
+++ angular/projects/global/src/lib/services/store.service.spec.ts
@@ -1,9 +1,13 @@
-import { TestBed } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 
 import { StoreService } from './store.service';
 
 describe('StoreService', () => {
-  beforeEach(() => TestBed.configureTestingModule({}));
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      providers: [StoreService]
+    });
+  }));
 
   it('should be created', () => {
     const service: StoreService = TestBed.get(StoreService);
