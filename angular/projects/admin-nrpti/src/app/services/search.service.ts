83ea852d466cb57ec89bf12747e0c70c8489bfac
Mark Lisé <mark@digitalspace.ca>
2019-12-03 14:51:18 -0800

    Adding updates, linting, import pages, list page, backend changes, records updates, model changes, merging in a bunch of EAGLE code.

---
+++ angular/projects/admin-nrpti/src/app/services/search.service.ts
@@ -0,0 +1,106 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+
+import { SearchResults } from '../models/search';
+
+@Injectable()
+export class SearchService {
+  public isError = false;
+
+  constructor(private http: HttpClient) {}
+
+  getItem(_id: string, schema: string): Observable<SearchResults[]> {
+    const queryString = `search?dataset=Item&_id=${_id}&_schemaName=${schema}`;
+    return this.http.get<SearchResults[]>(`${'http://localhost:3000/api'}/${queryString}`, {}).pipe(
+      map(res => {
+        const allResults = [] as any;
+        res.forEach(item => {
+          const r = new SearchResults({ type: item._schemaName, data: item });
+          allResults.push(r);
+        });
+        if (allResults.length === 1) {
+          return allResults[0];
+        } else {
+          return {};
+        }
+      })
+    );
+  }
+
+  // getFullList(schema: string): Observable<any> {
+  //   return this.api.getFullDataSet(schema);
+  // }
+
+  getSearchResults(
+    keys: string,
+    dataset: string,
+    fields: any[],
+    pageNum: number = 1,
+    pageSize: number = 10,
+    sortBy: string = null,
+    queryModifier: object = {},
+    populate: boolean = false,
+    filter: object = {}
+  ): Observable<any[]> {
+    if (sortBy === '') {
+      sortBy = null;
+    }
+    let queryString = `search?dataset=${dataset}`;
+    if (fields && fields.length > 0) {
+      fields.map(item => {
+        queryString += `&${item.name}=${item.value}`;
+      });
+    }
+    if (keys) {
+      queryString += `&keywords=${keys}`;
+    }
+    if (pageNum !== null) {
+      queryString += `&pageNum=${pageNum - 1}`;
+    }
+    if (pageSize !== null) {
+      queryString += `&pageSize=${pageSize}`;
+    }
+    if (sortBy !== '' && sortBy !== null) {
+      queryString += `&sortBy=${sortBy}`;
+    }
+    if (populate !== null) {
+      queryString += `&populate=${populate}`;
+    }
+    if (queryModifier !== {}) {
+      Object.keys(queryModifier).map(key => {
+        queryModifier[key].split(',').map(item => {
+          queryString += `&and[${key}]=${item}`;
+        });
+      });
+    }
+    if (filter !== {}) {
+      Object.keys(filter).map(key => {
+        filter[key].split(',').map(item => {
+          queryString += `&or[${key}]=${item}`;
+        });
+      });
+    }
+    queryString += `&fields=${this.buildValues(fields)}`;
+    return this.http.get<SearchResults[]>(`${'http://localhost:3000/api'}/${queryString}`, {}).pipe(
+      map(res => {
+        const allResults = [] as any;
+        res.forEach(item => {
+          const r = new SearchResults({ type: item._schemaName, data: item });
+          allResults.push(r);
+        });
+        return allResults;
+      })
+    );
+  }
+
+  private buildValues(collection: any[]): string {
+    let values = '';
+    collection.forEach(item => {
+      values += item + '|';
+    });
+    // trim the last |
+    return values.replace(/\|$/, '');
+  }
+}
---
+++ api/src/controllers/search.js
@@ -0,0 +1,660 @@
+var _ = require('lodash');
+var defaultLog = require('winston').loggers.get('default');
+var mongoose = require('mongoose');
+var Actions = require('../utils/actions');
+var Utils = require('../utils/utils');
+var qs = require('qs');
+
+function isEmpty(obj) {
+  for (var key in obj) {
+    if (obj.hasOwnProperty(key))
+      return false;
+  }
+  return true;
+}
+
+var generateExpArray = async function (field, roles) {
+  var expArray = [];
+  if (field && field != undefined) {
+    var queryString = qs.parse(field);
+    console.log("queryString:", queryString);
+    // Note that we need map and not forEach here because Promise.all uses
+    // the returned array!
+    await Promise.all(Object.keys(queryString).map(async item => {
+      var entry = queryString[item];
+      console.log("item:", item, entry);
+      if (item === 'pcp') {
+        await handlePCPItem(roles, expArray, entry);
+      } else if (Array.isArray(entry)) {
+        // Arrays are a list of options so will always be ors
+        var orArray = [];
+        entry.map(element => {
+          orArray.push(getConvertedValue(item, element));
+        });
+        expArray.push({ $or: orArray });
+      } else {
+        switch (item) {
+          case 'decisionDateStart':
+            handleDateStartItem(expArray, 'decisionDate', entry);
+            break;
+          case 'decisionDateEnd':
+            handleDateEndItem(expArray, 'decisionDate', entry);
+            break;
+          case 'datePostedStart':
+            handleDateStartItem(expArray, 'datePosted', entry);
+            break;
+          case 'datePostedEnd':
+            handleDateEndItem(expArray, 'datePosted', entry);
+            break;
+          default:
+            expArray.push(getConvertedValue(item, entry));
+            break;
+        }
+      }
+    }));
+  }
+  console.log("expArray:", expArray);
+  return expArray;
+}
+
+var getConvertedValue = function (item, entry) {
+  if (isNaN(entry)) {
+    if (mongoose.Types.ObjectId.isValid(entry)) {
+      console.log("objectid");
+      // ObjectID
+      return { [item]: mongoose.Types.ObjectId(entry) };
+    } else if (entry === 'true') {
+      console.log("bool");
+      // Bool
+      var tempObj = {}
+      tempObj[item] = true;
+      tempObj.active = true;
+      return tempObj;
+    } else if (entry === 'false') {
+      console.log("bool");
+      // Bool
+      return { [item]: false };
+    } else {
+      console.log("string");
+      return { [item]: entry };
+    }
+  } else {
+    console.log("number");
+    return { [item]: parseInt(entry) };
+  }
+}
+
+var handlePCPItem = async function (roles, expArray, value) {
+  if (Array.isArray(value)) {
+    // Arrays are a list of options so will always be ors
+    var orArray = [];
+    // Note that we need map and not forEach here because Promise.all uses
+    // the returned array!
+    await Promise.all(value.map(async entry => {
+      orArray.push(await getPCPValue(roles, entry));
+    }));
+    expArray.push({ $or: orArray });
+  } else {
+    expArray.push(await getPCPValue(roles, value));
+  }
+}
+
+var getPCPValue = async function (roles, entry) {
+  console.log('pcp: ', entry);
+
+  var query = null;
+  var now = new Date();
+
+  switch (entry) {
+    case 'pending':
+      var in7days = new Date();
+      in7days.setDate(now.getDate() + 7);
+
+      query = {
+        _schemaName: 'CommentPeriod',
+        $and: [
+          { dateStarted: { $gt: now } },
+          { dateStarted: { $lte: in7days } }
+        ]
+      };
+      break;
+
+    case 'open':
+      query = {
+        _schemaName: 'CommentPeriod',
+        $and: [
+          { dateStarted: { $lte: now } },
+          { dateCompleted: { $gt: now } }
+        ]
+      };
+      break;
+
+    case 'closed':
+      query = {
+        _schemaName: 'CommentPeriod',
+        dateCompleted: { $lt: now }
+      };
+      break;
+
+    default:
+      console.log('Unknown PCP entry');
+  }
+
+  var pcp = {};
+
+  if (query) {
+    var data = await Utils.runDataQuery('CommentPeriod', roles, query, ['project'], null, null, null, null, false, null);
+    var ids = _.map(data, 'project');
+    pcp = { _id: { $in: ids } };
+  }
+
+  console.log('pcp', pcp);
+  return pcp;
+}
+
+var handleDateStartItem = function (expArray, field, entry) {
+  var date = new Date(entry);
+
+  // Validate: valid date?
+  if (!isNaN(date)) {
+    var start = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate());
+    expArray.push({ [field]: { $gte: start } });
+  }
+}
+
+var handleDateEndItem = function (expArray, field, entry) {
+  var date = new Date(entry);
+
+  // Validate: valid date?
+  if (!isNaN(date)) {
+    var end = new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate() + 1);
+    expArray.push({ [field]: { $lt: end } });
+  }
+}
+
+var searchCollection = async function (roles, keywords, collection, pageNum, pageSize, project, sortField = undefined, sortDirection = undefined, caseSensitive, populate = false, and, or) {
+  var properties = undefined;
+  if (project) {
+    properties = { project: mongoose.Types.ObjectId(project) };
+  }
+
+  // optional search keys
+  var searchProperties = undefined;
+  if (keywords) {
+    searchProperties = { $text: { $search: keywords, $caseSensitive: caseSensitive } };
+  }
+
+  // query modifiers
+  var andExpArray = await generateExpArray(and, roles);
+
+  // filters
+  var orExpArray = await generateExpArray(or, roles);
+
+  var modifier = {};
+  if (andExpArray.length > 0 && orExpArray.length > 0) {
+    modifier = { $and: [{ $and: andExpArray }, { $and: orExpArray }] };
+  } else if (andExpArray.length === 0 && orExpArray.length > 0) {
+    modifier = { $and: orExpArray };
+  } else if (andExpArray.length > 0 && orExpArray.length === 0) {
+    modifier = { $and: andExpArray };
+  }
+
+  var match = {
+    _schemaName: collection,
+    ...(isEmpty(modifier) ? undefined : modifier),
+    ...(searchProperties ? searchProperties : undefined),
+    ...(properties ? properties : undefined),
+    $or: [
+      { isDeleted: { $exists: false } },
+      { isDeleted: false },
+    ]
+  };
+
+  console.log("modifier:", modifier);
+  console.log("match:", match);
+
+  var sortingValue = {};
+  sortingValue[sortField] = sortDirection;
+
+  let searchResultAggregation = [];
+  // We don't want to have sort in the aggregation if the front end doesn't need sort.
+  if (sortField && sortDirection) {
+    searchResultAggregation.push(
+      {
+        $sort: sortingValue
+      }
+    );
+  }
+  searchResultAggregation.push(
+    {
+      $skip: pageNum * pageSize
+    },
+    {
+      $limit: pageSize
+    }
+  );
+
+
+  var aggregation = [
+    {
+      $match: match
+    }
+  ];
+
+  let collation = {
+    locale: 'en',
+    strength: 2
+  };
+
+  console.log('collation:', collation);
+
+  if (collection === 'Document') {
+    // Allow documents to be sorted by status based on publish existence
+    aggregation.push(
+      {
+        $addFields: {
+          "status": {
+            $cond: {
+              if: {
+                // This way, if read isn't present, we assume public no roles array.
+                $and: [
+                  { $cond: { if: "$read", then: true, else: false } },
+                  {
+                    $anyElementTrue: {
+                      $map: {
+                        input: "$read",
+                        as: "fieldTag",
+                        in: { $setIsSubset: [["$$fieldTag"], ['public']] }
+                      }
+                    }
+                  }
+                ]
+              },
+              then: 'published',
+              else: 'unpublished'
+            }
+          }
+        }
+      }
+    );
+  }
+
+  if (collection === 'Project') {
+    // pop proponent if exists.
+    aggregation.push(
+      {
+        '$lookup': {
+          "from": "epic",
+          "localField": "proponent",
+          "foreignField": "_id",
+          "as": "proponent"
+        }
+      });
+    aggregation.push(
+      {
+        "$unwind": "$proponent"
+      },
+    );
+  }
+
+  if (collection === 'Group') {
+    // pop project and user if exists.
+    aggregation.push(
+      {
+        '$lookup': {
+          "from": "epic",
+          "localField": "project",
+          "foreignField": "_id",
+          "as": "project"
+        }
+      });
+    aggregation.push(
+      {
+        "$unwind": "$project"
+      },
+    );
+  }
+
+  if (collection === 'User') {
+    // pop proponent if exists.
+    aggregation.push(
+      {
+        '$lookup': {
+          "from": "epic",
+          "localField": "org",
+          "foreignField": "_id",
+          "as": "org"
+        }
+      });
+    aggregation.push(
+      {
+        "$unwind": "$org"
+      },
+    );
+  }
+
+  console.log('populate:', populate);
+  if (populate === true && collection !== 'Project') {
+    aggregation.push({
+      "$lookup": {
+        "from": "epic",
+        "localField": "project",
+        "foreignField": "_id",
+        "as": "project"
+      }
+    });
+    aggregation.push({
+      "$addFields": {
+        project: "$project",
+      }
+    });
+    aggregation.push({
+      "$unwind": {
+        "path": "$project",
+        "preserveNullAndEmptyArrays": true
+      }
+    });
+  }
+
+  if (populate === true && collection === 'Inspection') {
+    // pop elements and their items.
+    aggregation.push(
+      {
+        '$lookup': {
+          "from": "epic",
+          "localField": "elements",
+          "foreignField": "_id",
+          "as": "elements"
+        }
+      }
+    );
+  } else if (populate === true && collection === 'InspectionElement') {
+    aggregation.push(
+      {
+        '$lookup': {
+          "from": "epic",
+          "localField": "items",
+          "foreignField": "_id",
+          "as": "items"
+        }
+      }
+    );
+  }
+
+  if (populate === true && collection === 'NotificationProject') {
+    aggregation.push(
+      {
+        $lookup: {
+          from: 'epic',
+          as: 'documents',
+          let: { project: "$_id", schema: 'Document' },
+          pipeline: [
+            {
+              $match: {
+                $expr: {
+                  $and: [
+                    { $eq: ['$project', '$$project'] },
+                    { $eq: ['$_schemaName', 'Document'] }
+                  ]
+                }
+              }
+            },
+            {
+              $redact: {
+                $cond: {
+                  if: {
+                    // This way, if read isn't present, we assume public no roles array.
+                    $and: [
+                      { $cond: { if: "$read", then: true, else: false } },
+                      {
+                        $anyElementTrue: {
+                          $map: {
+                            input: "$read",
+                            as: "fieldTag",
+                            in: { $setIsSubset: [["$$fieldTag"], ['public']] }
+                          }
+                        }
+                      }
+                    ]
+                  },
+                  then: "$$KEEP",
+                  else: {
+                    $cond: { if: "$read", then: "$$PRUNE", else: "$$DESCEND" }
+                  }
+                }
+              }
+            }
+          ]
+        }
+      }
+    );
+  }
+
+  aggregation.push({
+    $redact: {
+      $cond: {
+        if: {
+          // This way, if read isn't present, we assume public no roles array.
+          $and: [
+            { $cond: { if: "$read", then: true, else: false } },
+            {
+              $anyElementTrue: {
+                $map: {
+                  input: "$read",
+                  as: "fieldTag",
+                  in: { $setIsSubset: [["$$fieldTag"], roles] }
+                }
+              }
+            }
+          ]
+        },
+        then: "$$KEEP",
+        else: {
+          $cond: { if: "$read", then: "$$PRUNE", else: "$$DESCEND" }
+        }
+      }
+    }
+  });
+
+  aggregation.push({
+    $addFields: {
+      score: { $meta: "textScore" }
+    }
+  });
+
+  aggregation.push({
+    $facet: {
+      searchResults: searchResultAggregation,
+      meta: [
+        {
+          $count: "searchResultsTotal"
+        }
+      ]
+    }
+  })
+
+  return new Promise(function (resolve, reject) {
+    var collectionObj = mongoose.model(collection);
+    collectionObj.aggregate(aggregation)
+      .collation(collation)
+      .exec()
+      .then(function (data) {
+        resolve(Utils.filterData(collection, data, roles));
+      }, reject);
+  });
+}
+
+exports.publicGet = async function (args, res, next) {
+  executeQuery(args, res, next);
+};
+
+exports.protectedGet = function (args, res, next) {
+  executeQuery(args, res, next);
+};
+
+var executeQuery = async function (args, res, next) {
+  var _id = args.swagger.params._id ? args.swagger.params._id.value : null;
+  var keywords = args.swagger.params.keywords.value;
+  var dataset = args.swagger.params.dataset.value;
+  var project = args.swagger.params.project.value;
+  var populate = args.swagger.params.populate ? args.swagger.params.populate.value : false;
+  var pageNum = args.swagger.params.pageNum.value || 0;
+  var pageSize = args.swagger.params.pageSize.value || 25;
+  var sortBy = args.swagger.params.sortBy.value ? args.swagger.params.sortBy.value : keywords ? ['-score'] : [];
+  var caseSensitive = args.swagger.params.caseSensitive ? args.swagger.params.caseSensitive.value : false;
+  var and = args.swagger.params.and ? args.swagger.params.and.value : '';
+  var or = args.swagger.params.or ? args.swagger.params.or.value : '';
+  defaultLog.info("Searching keywords:", keywords);
+  defaultLog.info("Searching datasets:", dataset);
+  defaultLog.info("Searching project:", project);
+  defaultLog.info("pageNum:", pageNum);
+  defaultLog.info("pageSize:", pageSize);
+  defaultLog.info("sortBy:", sortBy);
+  defaultLog.info("caseSensitive:", caseSensitive);
+  defaultLog.info("and:", and);
+  defaultLog.info("or:", or);
+  defaultLog.info("_id:", _id);
+  defaultLog.info("populate:", populate);
+
+  var roles = args.swagger.params.auth_payload ? args.swagger.params.auth_payload.realm_access.roles : ['public'];
+
+  console.log("Searching Collection:", dataset);
+
+  console.log("******************************************************************");
+  console.log(roles);
+  console.log("******************************************************************");
+
+  Utils.recordAction('Search', keywords, args.swagger.params.auth_payload ? args.swagger.params.auth_payload.preferred_username : 'public')
+
+  var sortDirection = undefined;
+  var sortField = undefined;
+
+  var sortingValue = {};
+  sortBy.map((value) => {
+    sortDirection = value.charAt(0) == '-' ? -1 : 1;
+    sortField = value.slice(1);
+    sortingValue[sortField] = sortDirection;
+  });
+
+  console.log("sortingValue:", sortingValue);
+  defaultLog.info("sortField:", sortField);
+  defaultLog.info("sortDirection:", sortDirection);
+
+  if (dataset !== 'Item') {
+
+    console.log("Searching Collection:", dataset);
+    console.log("sortField:", sortField);
+    var itemData = await searchCollection(roles, keywords, dataset, pageNum, pageSize, project, sortField, sortDirection, caseSensitive, populate, and, or)
+    if (dataset === 'Comment') {
+      // Filter
+      _.each(itemData[0].searchResults, function (item) {
+        if (item.isAnonymous === true) {
+          delete item.author;
+        }
+      });
+    }
+    return Actions.sendResponse(res, 200, itemData);
+
+  } else if (dataset === 'Item') {
+    var collectionObj = mongoose.model(args.swagger.params._schemaName.value);
+    console.log("ITEM GET", { _id: args.swagger.params._id.value })
+
+    let aggregation = [
+      {
+        "$match": { _id: mongoose.Types.ObjectId(args.swagger.params._id.value) }
+      },
+      {
+        $redact: {
+          $cond: {
+            if: {
+              // This way, if read isn't present, we assume public no roles array.
+              $and: [
+                { $cond: { if: "$read", then: true, else: false } },
+                {
+                  $anyElementTrue: {
+                    $map: {
+                      input: "$read",
+                      as: "fieldTag",
+                      in: { $setIsSubset: [["$$fieldTag"], roles] }
+                    }
+                  }
+                }
+              ]
+            },
+            then: "$$KEEP",
+            else: {
+              $cond: { if: "$read", then: "$$PRUNE", else: "$$DESCEND" }
+            }
+          }
+        }
+      }
+    ];
+
+    if (args.swagger.params._schemaName.value === 'Inspection') {
+      // pop elements and their items.
+      aggregation.push(
+        {
+          '$lookup': {
+            "from": "epic",
+            "localField": "elements",
+            "foreignField": "_id",
+            "as": "elements"
+          }
+        }
+      );
+      aggregation.push({
+        "$lookup": {
+          "from": "epic",
+          "localField": "project",
+          "foreignField": "_id",
+          "as": "project"
+        }
+      });
+      aggregation.push({
+        "$addFields": {
+          project: "$project",
+        }
+      });
+      aggregation.push({
+        "$unwind": {
+          "path": "$project",
+          "preserveNullAndEmptyArrays": true
+        }
+      });
+    } else if (args.swagger.params._schemaName.value === 'InspectionElement') {
+      aggregation.push(
+        {
+          '$lookup': {
+            "from": "epic",
+            "localField": "items",
+            "foreignField": "_id",
+            "as": "items"
+          }
+        }
+      );
+    }
+    var data = await collectionObj.aggregate(aggregation);
+
+    if (args.swagger.params._schemaName.value === 'Comment') {
+      // Filter
+      _.each(data, function (item) {
+        if (item.isAnonymous === true) {
+          delete item.author;
+        }
+      });
+    }
+
+    if (args.swagger.params._schemaName.value === 'Project') {
+      // If we are a project, and we are not authed, we need to sanitize some fields.
+      data = Utils.filterData(args.swagger.params._schemaName.value, data, roles);
+    }
+    return Actions.sendResponse(res, 200, data);
+  } else {
+    console.log('Bad Request');
+    return Actions.sendResponse(res, 400, {});
+  }
+};
+
+exports.protectedOptions = function (args, res, next) {
+  res.status(200).send();
+};
