86e82ac9d28f78231fe23b154252dca6d353ef11
Daniel Truong <daniel@dtsoftware.ca>
2019-12-13 15:49:11 -0800

    Updated breadcrumbs (#57)

--- angular/projects/admin-nrpti/src/app/app.component.ts
+++ angular/projects/admin-nrpti/src/app/app.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import { IBreadcrumb, StoreService } from 'nrpti-angular-components';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  constructor() {}
+export class AppComponent implements OnInit {
+  public breadcrumbs: IBreadcrumb[];
+  public activeBreadcrumb: IBreadcrumb;
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private router: Router,
+    private storeService: StoreService,
+    private changeDetectionRef: ChangeDetectorRef
+  ) {
+    this.breadcrumbs = [];
+  }
+
+  ngOnInit() {
+    // Subscribe to the NavigationEnd event
+    this.router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(event => {
+        // Set breadcrumbs
+        const root: ActivatedRoute = this.activatedRoute.root;
+        this.breadcrumbs = this.storeService.getBreadcrumbs(root);
+        this.activeBreadcrumb = this.breadcrumbs.pop();
+        this.changeDetectionRef.detectChanges();
+      });
+  }
 }
