cf8ba408b1e1a904a8461adaa7483c9588879097
Mark Lisé <mark@digitalspace.ca>
2019-12-16 13:19:16 -0800

    Bringing in sidebar nav.

---
+++ angular/projects/admin-nrpti/src/app/toggle-button/toggle-button.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+import { StoreService } from 'nrpti-angular-components';
+
+@Component({
+  selector: 'app-toggle-button',
+  templateUrl: './toggle-button.component.html',
+  styleUrls: ['./toggle-button.component.scss']
+})
+
+export class ToggleButtonComponent {
+
+  public loading = true;
+  public classApplied = false;
+
+  constructor(
+    private storeService: StoreService
+  ) { }
+
+  toggleSideNav() {
+    this.storeService.toggle();
+    this.classApplied = !this.classApplied;
+  }
+
+}
