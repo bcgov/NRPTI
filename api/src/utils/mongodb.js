e3bade16fb881855125071445c654b84d150344c
Mark Lisé <mark@digitalspace.ca>
2019-12-23 13:58:06 -0800

    Update conn string

--- api/src/utils/mongodb.js
+++ api/src/utils/mongodb.js
@@ -4,10 +4,19 @@ function MongoDriver(options) {
   this.connections = [];
 
   let MongoClient = require('mongodb').MongoClient;
-  let DB_CONNECTION = process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_USERNAME + '@mongodb://' + (process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost');
+
+  let DB_CONNECTION = '';
   let self = this;
 
-  MongoClient.connect(DB_CONNECTION, { uri_decode_auth: true }, function(err, db) {
+  if (process.env.MONGODB_USERNAME && process.env.MONGODB_PASSWORD) {
+    DB_CONNECTION = 'mongodb://' + process.env.MONGODB_USERNAME + ':' + process.env.MONGODB_PASSWORD + '@' + (process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost');
+  } else {
+    DB_CONNECTION = 'mongodb://' + (process.env.MONGODB_SERVICE_HOST || process.env.DB_1_PORT_27017_TCP_ADDR || 'localhost');
+  }
+
+  MongoClient.connect(DB_CONNECTION, function(err, db) {
+    console.log('db err:', err);
+    console.log('db:', db);
     if (err === null) {
       self.connections.push(db);
     }
