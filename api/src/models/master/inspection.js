8b50733088d38c66a9435edc928c2e098abe9c35
Daniel Truong <daniel@dtsoftware.ca>
2020-01-07 11:44:32 -0800

    Added sourceSystemRef to inspections (#82)

--- api/src/models/master/inspection.js
+++ api/src/models/master/inspection.js
@@ -29,7 +29,8 @@ module.exports = require('../../utils/model-schema-generator')(
     updatedBy: { type: String, default: '' },
     publishedBy: { type: String, default: '' },
     sourceDateAdded: { type: Date, default: null },
-    sourceDateUpdated: { type: Date, default: null }
+    sourceDateUpdated: { type: Date, default: null },
+    sourceSystemRef: { type: String, default: '' }
   },
   'nrpti'
 );
