a66ca23824debcbda4673e71309b1565ca154235
Nick Phura <Nick.Phura@quartech.com>
2020-02-04 20:49:42 -0800

    PTI-383: Add Restorative Justice NRCED Details expand

---
+++ angular/projects/public-nrpti/src/app/records/restorative-justices/restorative-justice-resolver.ts
@@ -0,0 +1,14 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, Resolve } from '@angular/router';
+import { Observable } from 'rxjs/Observable';
+import { FactoryService } from '../../services/factory.service';
+
+@Injectable()
+export class RestorativeJusticeResolver implements Resolve<Observable<object>> {
+  constructor(private factoryService: FactoryService) {}
+
+  resolve(route: ActivatedRouteSnapshot): Observable<object> {
+    const restorativeJusticeId = route.paramMap.get('restorativeJusticeId');
+    return this.factoryService.getRecord(restorativeJusticeId, 'RestorativeJusticeNRCED');
+  }
+}
