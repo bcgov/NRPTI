ef9ec24908baf5f41f3f2e45d414fb59021db418
Nick Phura <Nick.Phura@quartech.com>
2020-02-13 21:06:15 -0800

    PTI-387: Add auto close side content behaviour.

--- angular/projects/admin-nrpti/src/app/app.component.ts
+++ angular/projects/admin-nrpti/src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, HostListener, ChangeDetectorRef } from '@angular/core';
 import { Router } from '@angular/router';
 import { IBreadcrumb, StoreService } from 'nrpti-angular-components';
 
@@ -8,21 +8,61 @@ import { IBreadcrumb, StoreService } from 'nrpti-angular-components';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements OnInit {
-  public isOpen = true;
+  public currentWindowWidth = window.innerWidth;
+  public showSideContent = this.currentWindowWidth > 768 ? true : false;
+  public userClosedSideContent = false;
+
   public breadcrumbs: IBreadcrumb[];
   public activeBreadcrumb: IBreadcrumb;
 
-  constructor(private router: Router, private storeService: StoreService) {
+  constructor(
+    private router: Router,
+    private storeService: StoreService,
+    private _changeDetectionRef: ChangeDetectorRef
+  ) {
     this.breadcrumbs = [];
+  }
+
+  ngOnInit() {
+    this.storeService.stateChange.subscribe((state: object) => {
+      if (state && state.hasOwnProperty('userClosedSideContent')) {
+        this.userClosedSideContent = state['userClosedSideContent'];
+      }
+
+      if (state && state.hasOwnProperty('showSideContent')) {
+        this.showSideContent = state['showSideContent'];
+      }
+
+      this._changeDetectionRef.detectChanges();
+    });
   }
 
   public navigateBreadcrumb(breadcrumbData) {
     this.router.navigate([breadcrumbData.url]);
   }
 
-  ngOnInit() {
-    this.storeService.change.subscribe(isOpen => {
-      this.isOpen = isOpen;
-    });
+  @HostListener('window:resize', []) onResize() {
+    this.screenSizeChanged();
+  }
+
+  public screenSizeChanged() {
+    const newWindowWidth = window.innerWidth;
+
+    // only trigger auto open/close behaviour if the user hasn't manually closed the side content
+    if (!this.userClosedSideContent) {
+      if (this.currentWindowWidth > 768 && newWindowWidth <= 768) {
+        // window size changed from large to small: hide the side content
+        this.showSideContent = false;
+        this.storeService.setItem({ showSideContent: false });
+      } else if (this.currentWindowWidth <= 768 && newWindowWidth > 768) {
+        // window size changes from small to large: show the side content
+        this.showSideContent = true;
+        this.storeService.setItem({ showSideContent: true });
+      }
+
+      this._changeDetectionRef.detectChanges();
+    }
+
+    this.currentWindowWidth = window.innerWidth;
   }
 }
