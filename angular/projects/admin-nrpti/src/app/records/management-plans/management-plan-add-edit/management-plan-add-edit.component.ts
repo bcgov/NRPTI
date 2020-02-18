34133cc8b2b7a1459c3354b9f52ae457ce680526
Nick Phura <Nick.Phura@quartech.com>
2020-02-18 11:04:18 -0800

    PTI-313-2: Add missing null check

--- angular/projects/admin-nrpti/src/app/records/management-plans/management-plan-add-edit/management-plan-add-edit.component.ts
+++ angular/projects/admin-nrpti/src/app/records/management-plans/management-plan-add-edit/management-plan-add-edit.component.ts
@@ -156,7 +156,7 @@ export class ManagementPlanAddEditComponent implements OnInit, OnDestroy {
       centroid: [this.myForm.controls.latitude.value, this.myForm.controls.longitude.value],
       outcomeStatus: this.myForm.controls.outcomeStatus.value,
       outcomeDescription: this.myForm.controls.outcomeDescription.value,
-      attachments: this.currentRecord.attachments
+      attachments: this.currentRecord && this.currentRecord.attachments
     });
 
     // Project name logic
