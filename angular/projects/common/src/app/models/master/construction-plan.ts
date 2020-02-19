f5a1a6b0f36a9cf3019bb1f5b38b7082559e0847
Daniel Truong <daniel@dtsoftware.ca>
2020-02-19 15:11:25 -0800

    Updated common models to have documents field

--- angular/projects/common/src/app/models/master/construction-plan.ts
+++ angular/projects/common/src/app/models/master/construction-plan.ts
@@ -22,7 +22,7 @@
   projectName: string;
   location: string;
   centroid: number[];
-  attachments: object[];
+  documents: object[];
 
   dateAdded: Date;
   dateUpdated: Date;
@@ -52,7 +52,7 @@
     this.projectName = (obj && obj.projectName) || null;
     this.location = (obj && obj.location) || null;
     this.centroid = (obj && obj.centroid) || null;
-    this.attachments = (obj && obj.attachments) || null;
+    this.documents = (obj && obj.documents) || null;
 
     this.dateAdded = (obj && obj.dateAdded) || null;
     this.dateUpdated = (obj && obj.dateUpdated) || null;
