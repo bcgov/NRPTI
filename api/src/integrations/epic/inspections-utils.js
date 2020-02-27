29ff4f39e96ef6bf77c69abd79ebef28fb3113ed
Nick Phura <Nick.Phura@quartech.com>
2020-02-26 18:02:15 -0800

    PTI-330: Pre-work: Reduce code duplication in epic import.

--- api/src/integrations/epic/inspections-utils.js
+++ api/src/integrations/epic/inspections-utils.js
@@ -1,29 +1,22 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const defaultLog = require('../../utils/logger')('epic-inspections');
-const RECORD_TYPE = require('../../utils/constants/record-type-enum');
-const EpicUtils = require('./epic-utils');
-const DocumentController = require('./../../controllers/document-controller');
-
+const BaseRecordUtils = require('./base-record-utils');
 /**
- * Epic Inspection record handler for { type: 'Inspection Record', milestone: 'Compliance & Enforcement' }.
- *
- * Must contain the following functions:
- * - transformRecord: (object) => Inspection
- * - saveRecord: (Inspection) => any
+ * Epic Inspection record handler for:
+ *  - { type: 'Inspection Record', milestone: 'Compliance & Enforcement' }.
  *
  * @class Inspections
  */
-class Inspections {
+class Inspections extends BaseRecordUtils {
   /**
    * Creates an instance of Inspections.
    *
    * @param {*} auth_payload user information for auditing
+   * @param {*} recordType an item from record-type-enum.js -> RECORD_TYPE
    * @memberof Inspections
    */
-  constructor(auth_payload) {
-    this.auth_payload = auth_payload;
+  constructor(auth_payload, recordType) {
+    super(auth_payload, recordType);
   }
 
   /**
@@ -39,80 +32,14 @@ class Inspections {
       throw Error('transformRecord - required record must be non-null.');
     }
 
-    // Apply common Epic pre-processing/transformations
-    epicRecord = EpicUtils.preTransformRecord(epicRecord);
-
-    // Creating and saving a document object if we are given a link to an EPIC document.
-    const documents = [];
-    if (epicRecord._id && epicRecord.documentFileName) {
-      const savedDocument = await DocumentController.createDocument(
-        epicRecord.documentFileName,
-        (this.auth_payload && this.auth_payload.displayName) || '',
-        `https://projects.eao.gov.bc.ca/api/document/${epicRecord._id}/fetch/${encodeURIComponent(
-          epicRecord.documentFileName
-        )}`
-      );
-      documents.push(savedDocument);
-    }
-
     return {
-      _schemaName: RECORD_TYPE.Inspection._schemaName,
-      _epicProjectId: (epicRecord.project && epicRecord.project._id) || '',
-      _sourceRefId: epicRecord._id || '',
-      _epicMilestoneId: epicRecord.milestone || '',
-
-      read: ['sysadmin'],
-      write: ['sysadmin'],
-
-      recordName: epicRecord.displayName || '',
-      recordType: RECORD_TYPE.Inspection.displayName,
-      dateIssued: epicRecord.datePosted || null,
+      ...(await super.transformRecord(epicRecord)),
       issuingAgency: 'Environmental Assessment Office',
       author: epicRecord.documentAuthor || '',
-      description: epicRecord.description || '',
       legislation: {
         act: (epicRecord.project && epicRecord.project.legislation) || ''
-      },
-      projectName: (epicRecord.project && epicRecord.project.name) || '',
-      location: (epicRecord.project && epicRecord.project.location) || '',
-      centroid: (epicRecord.project && epicRecord.project.centroid) || '',
-      documents: documents,
-
-      dateAdded: new Date(),
-      dateUpdated: new Date(),
-      updatedBy: (this.auth_payload && this.auth_payload.displayName) || '',
-      sourceDateAdded: epicRecord.dateAdded || epicRecord._createdDate || null,
-      sourceDateUpdated: epicRecord.dateUpdated || epicRecord._updatedDate || null,
-      sourceSystemRef: 'epic'
+      }
     };
-  }
-
-  /**
-   * Persist a NRPTI Inspection record to the database.
-   *
-   * @async
-   * @param {Inspection} inspectionRecord NRPTI Inspection record (required)
-   * @returns {string} status of the add/update operations.
-   * @memberof Inspections
-   */
-  async saveRecord(inspectionRecord) {
-    if (!inspectionRecord) {
-      throw Error('saveRecord - required record must be non-null.');
-    }
-
-    try {
-      const Inspection = mongoose.model(RECORD_TYPE.Inspection._schemaName);
-
-      const record = await Inspection.findOneAndUpdate(
-        { _schemaName: RECORD_TYPE.Inspection._schemaName, _sourceRefId: inspectionRecord._sourceRefId },
-        { $set: inspectionRecord },
-        { upsert: true, new: true }
-      );
-
-      return record;
-    } catch (error) {
-      defaultLog.error(`Failed to save Epic Inspection record: ${error.message}`);
-    }
   }
 }
 
--- api/src/integrations/epic/orders-other-utils.js
+++ api/src/integrations/epic/orders-other-utils.js
@@ -1,29 +1,22 @@
 'use strict';
 
-const mongoose = require('mongoose');
-const defaultLog = require('../../utils/logger')('epic-orders');
-const RECORD_TYPE = require('../../utils/constants/record-type-enum');
-const EpicUtils = require('./epic-utils');
-const DocumentController = require('./../../controllers/document-controller');
-
+const Orders = require('./orders-utils');
 /**
- * Epic Order record handler for { type: 'Order', milestone: 'Other' }.
- *
- * Must contain the following functions:
- * - transformRecord: (object) => Order
- * - saveRecord: (Order) => any
+ * Epic Orders record handler for:
+ *  - { type: 'Order', milestone: 'Other' }.
  *
  * @class OrdersOther
  */
-class OrdersOther {
+class OrdersOther extends Orders {
   /**
    * Creates an instance of OrdersOther.
    *
    * @param {*} auth_payload user information for auditing
+   * @param {*} recordType an item from record-type-enum.js -> RECORD_TYPE
    * @memberof OrdersOther
    */
-  constructor(auth_payload) {
-    this.auth_payload = auth_payload;
+  constructor(auth_payload, recordType) {
+    super(auth_payload, recordType);
   }
 
   /**
@@ -39,81 +32,10 @@ class OrdersOther {
       throw Error('transformRecord - required record must be non-null.');
     }
 
-    // Apply common Epic pre-processing/transformations
-    epicRecord = EpicUtils.preTransformRecord(epicRecord);
-
-    // Creating and saving a document object if we are given a link to an EPIC document.
-    const documents = [];
-    if (epicRecord._id && epicRecord.documentFileName) {
-      const savedDocument = await DocumentController.createDocument(
-        epicRecord.documentFileName,
-        (this.auth_payload && this.auth_payload.displayName) || '',
-        `https://projects.eao.gov.bc.ca/api/document/${epicRecord._id}/fetch/${encodeURIComponent(
-          epicRecord.documentFileName
-        )}`
-      );
-      documents.push(savedDocument);
-    }
-
     return {
-      _schemaName: RECORD_TYPE.Order._schemaName,
-      _epicProjectId: (epicRecord.project && epicRecord.project._id) || '',
-      _sourceRefId: epicRecord._id || '',
-      _epicMilestoneId: epicRecord.milestone || '',
-
-      read: ['sysadmin'],
-      write: ['sysadmin'],
-
-      recordName: epicRecord.displayName || '',
-      recordType: RECORD_TYPE.Order.displayName,
-      dateIssued: epicRecord.documentDate || null,
-      issuingAgency: 'Environmental Assessment Office',
-      author: epicRecord.documentAuthor || '',
-      description: epicRecord.description || '',
-      legislation: {
-        act: (epicRecord.project && epicRecord.project.legislation) || ''
-      },
-      projectName: (epicRecord.project && epicRecord.project.name) || '',
-      location: (epicRecord.project && epicRecord.project.location) || '',
-      centroid: (epicRecord.project && epicRecord.project.centroid) || '',
-      documents: documents,
-
-      dateAdded: new Date(),
-      dateUpdated: new Date(),
-      updatedBy: (this.auth_payload && this.auth_payload.displayName) || '',
-      sourceDateAdded: epicRecord.dateAdded || epicRecord._createdDate || null,
-      sourceDateUpdated: epicRecord.dateUpdated || epicRecord._updatedDate || null,
-      sourceSystemRef: 'epic'
+      ...(await super.transformRecord(epicRecord)),
+      recordSubtype: 'Other'
     };
-  }
-
-  /**
-   * Persist a NRPTI Order record to the database.
-   *
-   * @async
-   * @param {Order} orderRecord NRPTI Order record (required)
-   * @returns {string} status of the add/update operations.
-   * @memberof OrdersOther
-   */
-  async saveRecord(orderRecord) {
-    if (!orderRecord) {
-      throw Error('saveRecord - required record must be non-null.');
-    }
-
-    try {
-      const Order = mongoose.model(RECORD_TYPE.Order._schemaName);
-
-      const record = await Order.findOneAndUpdate(
-        { _schemaName: RECORD_TYPE.Order._schemaName, _sourceRefId: orderRecord._sourceRefId },
-        { $set: orderRecord },
-        { upsert: true, new: true }
-      );
-
-      return record;
-    } catch (error) {
-      defaultLog.error(`Failed to save Epic Order record: ${error.message}`);
-      defaultLog.debug(`Failed to save Epic Order record - error.stack: ${error.stack}`);
-    }
   }
 }
 
