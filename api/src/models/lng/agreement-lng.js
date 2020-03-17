672068d12820231d054984a57e89201b5485f2e7
Nick Phura <nickphura@gmail.com>
2020-03-16 23:27:07 -0700

    PTI-411: Update Api - agreement

--- api/src/models/lng/agreement-lng.js
+++ api/src/models/lng/agreement-lng.js
@@ -4,17 +4,33 @@ module.exports = require('../../utils/model-schema-generator')(
   'AgreementLNG',
   {
     _schemaName: { type: String, default: 'AgreementLNG' },
+    _epicProjectId: { type: 'ObjectId', default: null, index: true },
+    _sourceRefId: { type: 'ObjectId', default: null, index: true },
+    _epicMilestoneId: { type: 'ObjectId', default: null, index: true },
 
     read: [{ type: String, trim: true, default: 'sysadmin' }],
     write: [{ type: String, trim: true, default: 'sysadmin' }],
 
-    _master: { type: 'ObjectId', ref: 'Agreement' },
+    recordName: { type: String, default: '' },
+    recordType: { type: String, default: '' },
+    dateIssued: { type: Date, default: null },
+    nationName: { type: String, default: '' },
+    projectName: { type: String, default: '' },
+    documents: [{ type: 'ObjectId', default: [], index: true }],
 
     description: { type: String, default: '' },
 
     dateAdded: { type: Date, default: Date.now() },
-    dateUpdated: { type: Date, default: Date.now() },
-    datePublished: { type: Date, default: Date.now() }
+    dateUpdated: { type: Date, default: null },
+    datePublished: { type: Date, default: null },
+
+    addedBy: { type: String, default: '' },
+    updatedBy: { type: String, default: '' },
+    publishedBy: { type: String, default: '' },
+
+    sourceDateAdded: { type: Date, default: null },
+    sourceDateUpdated: { type: Date, default: null },
+    sourceSystemRef: { type: String, default: 'nrpti' }
   },
   'nrpti'
 );
