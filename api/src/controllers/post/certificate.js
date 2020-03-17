dec42797d0f5d340459caa9be13e82a51a0192b4
Nick Phura <nickphura@gmail.com>
2020-03-16 23:27:30 -0700

    PTI-411: Update Api - certificate

--- api/src/controllers/post/certificate.js
+++ api/src/controllers/post/certificate.js
@@ -1,30 +1,105 @@
-const mongoose = require('mongoose');
-const ObjectId = require('mongoose').Types.ObjectId;
-const RECORD_TYPE = require('../../utils/constants/record-type-enum');
+let mongoose = require('mongoose');
+let ObjectId = require('mongoose').Types.ObjectId;
 
 /**
- * Create Master Certificate record.
+ * Performs all operations necessary to create a master Certificate record and its associated flavour records.
  *
- * Example of incomingObj:
+ * Example of incomingObj
  *
- * certificate: [
- *   {
- *     recordName: 'test abc',
- *     recordType: 'whatever',
- *     ...
- *     CertificateLNG: {
- *       description: 'lng description'
- *       addRole: 'public',
- *     }
- *   },
- *   ...
- * ]
+ *  certificates: [
+ *    {
+ *      recordName: 'test abc',
+ *      recordType: 'certificate',
+ *      ...
+ *      CertificateLNG: {
+ *        description: 'lng description'
+ *        addRole: 'public',
+ *        ...
+ *      }
+ *    }
+ *  ]
+ *
+ * @param {*} args
+ * @param {*} res
+ * @param {*} next
+ * @param {*} incomingObj see example
+ * @returns object containing the operation's status and created records
  */
-exports.createMaster = async function(args, res, next, incomingObj) {
-  const Certificate = mongoose.model(RECORD_TYPE.Certificate._schemaName);
-  const certificate = new Certificate();
+exports.createRecord = async function(args, res, next, incomingObj) {
+  // save flavour records
+  let observables = [];
+  let savedFlavourCertificates = [];
+  let flavourIds = [];
 
-  certificate._schemaName = RECORD_TYPE.Certificate._schemaName;
+  try {
+    incomingObj.CertificateLNG &&
+      observables.push(this.createLNG(args, res, next, { ...incomingObj, ...incomingObj.CertificateLNG }));
+
+    if (observables.length > 0) {
+      savedFlavourCertificates = await Promise.all(observables);
+
+      flavourIds = savedFlavourCertificates.map(flavourCertificate => flavourCertificate._id);
+    }
+  } catch (e) {
+    return {
+      status: 'failure',
+      object: savedFlavourCertificates,
+      errorMessage: e
+    };
+  }
+
+  // save certificate record
+  let savedCertificate = null;
+
+  try {
+    savedCertificate = await this.createMaster(args, res, next, incomingObj, flavourIds);
+
+    return {
+      status: 'success',
+      object: savedCertificate,
+      flavours: savedFlavourCertificates
+    };
+  } catch (e) {
+    return {
+      status: 'failure',
+      object: savedCertificate,
+      errorMessage: e
+    };
+  }
+};
+
+/**
+ * Performs all operations necessary to create a master Certificate record.
+ *
+ * Example of incomingObj
+ *
+ *  certificates: [
+ *    {
+ *      recordName: 'test abc',
+ *      recordType: 'certificate',
+ *      ...
+ *      CertificateLNG: {
+ *        description: 'lng description'
+ *        addRole: 'public',
+ *        ...
+ *      }
+ *    }
+ *  ]
+ *
+ * @param {*} args
+ * @param {*} res
+ * @param {*} next
+ * @param {*} incomingObj see example
+ * @param {*} flavourIds array of flavour record _ids
+ * @returns created master certificate record
+ */
+exports.createMaster = async function(args, res, next, incomingObj, flavourIds) {
+  let Certificate = mongoose.model('Certificate');
+  let certificate = new Certificate();
+
+  certificate._schemaName = 'Certificate';
+
+  // set integration references
   incomingObj._epicProjectId &&
     ObjectId.isValid(incomingObj._epicProjectId) &&
     (certificate._epicProjectId = new ObjectId(incomingObj._epicProjectId));
@@ -35,8 +110,23 @@ exports.createMaster = async function(args, res, next, incomingObj) {
     ObjectId.isValid(incomingObj._epicMilestoneId) &&
     (certificate._epicMilestoneId = new ObjectId(incomingObj._epicMilestoneId));
 
+  // set permissions
+  certificate.read = ['sysadmin'];
+  certificate.write = ['sysadmin'];
+
+  // set forward references
+  if (flavourIds && flavourIds.length) {
+    flavourIds.forEach(id => {
+      if (ObjectId.isValid(id)) {
+        certificate._flavourRecords.push(new ObjectId(id));
+      }
+    });
+  }
+
+  // set data
   incomingObj.recordName && (certificate.recordName = incomingObj.recordName);
-  certificate.recordType = RECORD_TYPE.Certificate.displayName;
+  certificate.recordType = 'Certificate';
+  incomingObj.recordSubtype && (certificate.recordSubtype = incomingObj.recordSubtype);
   incomingObj.dateIssued && (certificate.dateIssued = incomingObj.dateIssued);
   incomingObj.issuingAgency && (certificate.issuingAgency = incomingObj.issuingAgency);
   incomingObj.legislation && incomingObj.legislation.act && (certificate.legislation.act = incomingObj.legislation.act);
@@ -52,104 +142,109 @@ exports.createMaster = async function(args, res, next, incomingObj) {
   incomingObj.legislation &&
     incomingObj.legislation.paragraph &&
     (certificate.legislation.paragraph = incomingObj.legislation.paragraph);
-  incomingObj.issuedTo && (certificate.issuedTo = incomingObj.issuedTo);
   incomingObj.projectName && (certificate.projectName = incomingObj.projectName);
   incomingObj.location && (certificate.location = incomingObj.location);
   incomingObj.centroid && (certificate.centroid = incomingObj.centroid);
 
+  // set meta
+  certificate.addedBy = args.swagger.params.auth_payload.displayName;
   certificate.dateAdded = new Date();
-  certificate.publishedBy = args.swagger.params.auth_payload.displayName;
 
+  // set data source references
   incomingObj.sourceDateAdded && (certificate.sourceDateAdded = incomingObj.sourceDateAdded);
   incomingObj.sourceDateUpdated && (certificate.sourceDateUpdated = incomingObj.sourceDateUpdated);
   incomingObj.sourceSystemRef && (certificate.sourceSystemRef = incomingObj.sourceSystemRef);
 
-  certificate.read = ['sysadmin'];
-  certificate.write = ['sysadmin'];
-
-  let savedCertificate = null;
-  try {
-    savedCertificate = await certificate.save();
-  } catch (error) {
-    return {
-      status: 'failure',
-      object: certificate,
-      errorMessage: error.message
-    };
-  }
-
-  const observables = [];
-  incomingObj.CertificateLNG &&
-    observables.push(this.createLNG(args, res, next, incomingObj.CertificateLNG, savedCertificate._id));
-
-  let flavourRes = null;
-  try {
-    observables.length > 0 && (flavourRes = await Promise.all(observables));
-  } catch (error) {
-    flavourRes = {
-      status: 'failure',
-      object: observables,
-      errorMessage: error.message
-    };
-  }
-
-  return {
-    status: 'success',
-    object: savedCertificate,
-    flavours: flavourRes
-  };
+  return await certificate.save();
 };
 
 /**
- * Create LNG Certificate record.
+ * Performs all operations necessary to create a LNG Certificate record.
  *
- * Example of incomingObj:
+ * Example of incomingObj
  *
- * {
- *   _master: '5e1e7fcd20e4167bcfc3daa7'
- *   description: 'lng description',
- *   ...
- *   addRole: 'public'
- * }
+ *  certificates: [
+ *    {
+ *      recordName: 'test abc',
+ *      recordType: 'certificate',
+ *      ...
+ *      CertificateLNG: {
+ *        description: 'lng description'
+ *        addRole: 'public',
+ *        ...
+ *      }
+ *    }
+ *  ]
+ *
+ * @param {*} args
+ * @param {*} res
+ * @param {*} next
+ * @param {*} incomingObj see example
+ * @returns created lng certificate record
  */
-exports.createLNG = async function(args, res, next, incomingObj, masterId) {
-  // We must have a valid master ObjectID to continue.
-  if (!masterId || !ObjectId.isValid(masterId)) {
-    return {
-      status: 'failure',
-      object: incomingObj,
-      errorMessage: 'incomingObj._master was not valid ObjectId'
-    };
-  }
+exports.createLNG = async function(args, res, next, incomingObj) {
+  let CertificateLNG = mongoose.model('CertificateLNG');
+  let certificateLNG = new CertificateLNG();
 
-  const CertificateLNG = mongoose.model(`${RECORD_TYPE.Certificate._schemaName}LNG`);
-  const inpsectionLNG = new CertificateLNG();
+  certificateLNG._schemaName = 'CertificateLNG';
 
-  inpsectionLNG._schemaName = `${RECORD_TYPE.Certificate._schemaName}LNG`;
-  inpsectionLNG._master = new ObjectId(masterId);
-  inpsectionLNG.read = ['sysadmin'];
-  inpsectionLNG.write = ['sysadmin'];
+  // set integration references
+  incomingObj._epicProjectId &&
+    ObjectId.isValid(incomingObj._epicProjectId) &&
+    (certificateLNG._epicProjectId = new ObjectId(incomingObj._epicProjectId));
+  incomingObj._sourceRefId &&
+    ObjectId.isValid(incomingObj._sourceRefId) &&
+    (certificateLNG._sourceRefId = new ObjectId(incomingObj._sourceRefId));
+  incomingObj._epicMilestoneId &&
+    ObjectId.isValid(incomingObj._epicMilestoneId) &&
+    (certificateLNG._epicMilestoneId = new ObjectId(incomingObj._epicMilestoneId));
+
+  // set permissions and meta
+  certificateLNG.read = ['sysadmin'];
+  certificateLNG.write = ['sysadmin'];
+
   // If incoming object has addRole: 'public' then read will look like ['sysadmin', 'public']
-  incomingObj.addRole &&
-    incomingObj.addRole === 'public' &&
-    inpsectionLNG.read.push('public') &&
-    (inpsectionLNG.datePublished = new Date());
-
-  incomingObj.description && (inpsectionLNG.description = incomingObj.description);
-
-  inpsectionLNG.dateAdded = new Date();
-
-  try {
-    const savedCertificateLNG = await inpsectionLNG.save();
-    return {
-      status: 'success',
-      object: savedCertificateLNG
-    };
-  } catch (error) {
-    return {
-      status: 'failure',
-      object: inpsectionLNG,
-      errorMessage: error.message
-    };
+  if (incomingObj.addRole && incomingObj.addRole === 'public') {
+    certificateLNG.read.push('public');
+    certificateLNG.datePublished = new Date();
+    certificateLNG.publishedBy = args.swagger.params.auth_payload.displayName;
   }
+
+  certificateLNG.addedBy = args.swagger.params.auth_payload.displayName;
+  certificateLNG.dateAdded = new Date();
+
+  // set master data
+  incomingObj.recordName && (certificateLNG.recordName = incomingObj.recordName);
+  certificateLNG.recordType = 'Certificate';
+  incomingObj.recordSubtype && (certificateLNG.recordSubtype = incomingObj.recordSubtype);
+  incomingObj.dateIssued && (certificateLNG.dateIssued = incomingObj.dateIssued);
+  incomingObj.issuingAgency && (certificateLNG.issuingAgency = incomingObj.issuingAgency);
+  incomingObj.legislation &&
+    incomingObj.legislation.act &&
+    (certificateLNG.legislation.act = incomingObj.legislation.act);
+  incomingObj.legislation &&
+    incomingObj.legislation.regulation &&
+    (certificateLNG.legislation.regulation = incomingObj.legislation.regulation);
+  incomingObj.legislation &&
+    incomingObj.legislation.section &&
+    (certificateLNG.legislation.section = incomingObj.legislation.section);
+  incomingObj.legislation &&
+    incomingObj.legislation.subSection &&
+    (certificateLNG.legislation.subSection = incomingObj.legislation.subSection);
+  incomingObj.legislation &&
+    incomingObj.legislation.paragraph &&
+    (certificateLNG.legislation.paragraph = incomingObj.legislation.paragraph);
+  incomingObj.projectName && (certificateLNG.projectName = incomingObj.projectName);
+  incomingObj.location && (certificateLNG.location = incomingObj.location);
+  incomingObj.centroid && (certificateLNG.centroid = incomingObj.centroid);
+
+  // set flavour data
+  incomingObj.description && (certificateLNG.description = incomingObj.description);
+
+  // set data source references
+  incomingObj.sourceDateAdded && (certificateLNG.sourceDateAdded = incomingObj.sourceDateAdded);
+  incomingObj.sourceDateUpdated && (certificateLNG.sourceDateUpdated = incomingObj.sourceDateUpdated);
+  incomingObj.sourceSystemRef && (certificateLNG.sourceSystemRef = incomingObj.sourceSystemRef);
+
+  return await certificateLNG.save();
 };
