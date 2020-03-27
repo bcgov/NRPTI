3318eea66929004c208e7973b37ae9258f76097a
Nick Phura <nickphura@gmail.com>
2020-03-26 17:17:53 -0700

    PTI-411: Update common models for new arch/schemas

--- angular/projects/common/src/app/models/lng/management-plan-lng.ts
+++ angular/projects/common/src/app/models/lng/management-plan-lng.ts
@@ -7,31 +7,71 @@
 export class ManagementPlanLNG {
   _id: string;
   _schemaName: string;
-  _master: string;
+
+  _epicProjectId: string;
+  _sourceRefId: string;
+  _epicMilestoneId: string;
 
   read: string[];
   write: string[];
 
-  relatedPhase: string;
+  recordName: string;
+  recordType: string;
+  dateIssued: Date;
+  agency: string;
+  author: string;
+  projectName: string;
+  location: string;
+  centroid: number[];
+  documents: object[];
+
   description: string;
 
   dateAdded: Date;
   dateUpdated: Date;
   datePublished: Date;
 
+  addedBy: string;
+  updatedBy: string;
+  publishedBy: string;
+
+  sourceDateAdded: Date;
+  sourceDateUpdated: Date;
+  sourceSystemRef: string;
+
   constructor(obj?: any) {
     this._id = (obj && obj._id) || null;
     this._schemaName = (obj && obj._schemaName) || 'ManagementPlanLNG';
-    this._master = (obj && obj._master) || null;
+
+    this._epicProjectId = (obj && obj._epicProjectId) || '';
+    this._sourceRefId = (obj && obj._sourceRefId) || '';
+    this._epicMilestoneId = (obj && obj._epicMilestoneId) || '';
 
     this.read = (obj && obj.read) || null;
     this.write = (obj && obj.write) || null;
 
-    this.relatedPhase = (obj && obj.relatedPhase) || null;
+    this.recordName = (obj && obj.recordName) || '';
+    this.recordType = (obj && obj.recordType) || '';
+    this.dateIssued = (obj && obj.dateIssued) || null;
+    this.agency = (obj && obj.agency) || '';
+    this.author = (obj && obj.author) || '';
+    this.projectName = (obj && obj.projectName) || '';
+    this.location = (obj && obj.location) || '';
+    this.centroid = (obj && obj.centroid) || [];
+    this.documents = (obj && obj.documents) || [];
+
     this.description = (obj && obj.description) || null;
 
     this.dateAdded = (obj && obj.dateAdded) || null;
     this.dateUpdated = (obj && obj.dateUpdated) || null;
     this.datePublished = (obj && obj.datePublished) || null;
+
+    this.addedBy = (obj && obj.addedBy) || '';
+    this.updatedBy = (obj && obj.updatedBy) || '';
+    this.publishedBy = (obj && obj.publishedBy) || '';
+
+    this.sourceDateAdded = (obj && obj.sourceDateAdded) || null;
+    this.sourceDateUpdated = (obj && obj.sourceDateUpdated) || null;
+    this.sourceSystemRef = (obj && obj.sourceSystemRef) || '';
   }
 }
