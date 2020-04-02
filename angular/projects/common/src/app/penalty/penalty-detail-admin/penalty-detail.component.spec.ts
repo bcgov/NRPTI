7ef7cd614dc38c91907eadd57d019e16da8f9949
Nick Phura <nickphura@gmail.com>
2020-04-02 12:15:18 -0700

    PTI-298: Fix failing test.

--- angular/projects/common/src/app/penalty/penalty-detail-admin/penalty-detail.component.spec.ts
+++ angular/projects/common/src/app/penalty/penalty-detail-admin/penalty-detail.component.spec.ts
@@ -3,6 +3,7 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { MatAutocompleteModule } from '@angular/material';
 import { TestBedHelper } from '../../spec/spec-utils';
 import { PenaltyDetailComponent } from './penalty-detail.component';
+import { Penalty } from '../../models/master/common-models/penalty';
 
 describe('PenaltyDetailComponent', () => {
   const testBedHelper = new TestBedHelper<PenaltyDetailComponent>(PenaltyDetailComponent);
@@ -17,7 +18,9 @@ describe('PenaltyDetailComponent', () => {
   it('should create', () => {
     const { component, fixture } = testBedHelper.createComponent(false);
 
-    component.data = [{ type: 'convictionType', penalty: { type: 'Days', value: 140 }, description: 'description' }];
+    component.data = [
+      new Penalty({ type: 'convictionType', penalty: { type: 'Days', value: 140 }, description: 'description' })
+    ];
 
     fixture.detectChanges();
 
