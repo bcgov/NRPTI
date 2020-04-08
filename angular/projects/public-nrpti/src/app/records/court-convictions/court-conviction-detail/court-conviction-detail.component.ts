d5e2c4bfe52a0a27af7072490312ef214b48e0e6
Nick Phura <nickphura@gmail.com>
2020-04-07 18:50:54 -0700

    - Records List - no longer populate documents - Detail components - fetch documents on expand - UI - Fix vertical alignment of table values when collapsed - Remove unused route/resolver code

--- angular/projects/public-nrpti/src/app/records/court-convictions/court-conviction-detail/court-conviction-detail.component.ts
+++ angular/projects/public-nrpti/src/app/records/court-convictions/court-conviction-detail/court-conviction-detail.component.ts
@@ -2,7 +2,9 @@ import { Component, OnInit, ChangeDetectorRef, Input, OnDestroy } from '@angular
 import { Subject } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
 import { takeUntil } from 'rxjs/operators';
-import { CourtConvictionNRCED } from '../../../../../../common/src/app/models';
+import { CourtConvictionNRCED, Document } from '../../../../../../common/src/app/models';
+import { FactoryService } from '../../../services/factory.service';
+import { Utils as CommonUtils } from '../../../../../../common/src/app/utils/utils';
 
 @Component({
   selector: 'app-court-conviction-detail',
@@ -17,11 +19,19 @@ export class CourtConvictionDetailComponent implements OnInit, OnDestroy {
   public loading = true;
   public activeTab = 'detail';
 
-  constructor(public route: ActivatedRoute, public router: Router, public _changeDetectionRef: ChangeDetectorRef) {}
+  constructor(
+    public route: ActivatedRoute,
+    public router: Router,
+    public factoryService: FactoryService,
+    public _changeDetectionRef: ChangeDetectorRef
+  ) {}
 
   ngOnInit(): void {
     if (this.data) {
       this.data = new CourtConvictionNRCED(this.data);
+
+      // populate documents
+      this.getDocuments();
 
       this.loading = false;
       this._changeDetectionRef.detectChanges();
@@ -38,9 +48,38 @@ export class CourtConvictionDetailComponent implements OnInit, OnDestroy {
       // If data was passed in directly, take it over anything in the route resolver.
       this.data = (res.records[0] && res.records[0].data && new CourtConvictionNRCED(res.records[0].data)) || null;
 
+      // populate documents
+      this.getDocuments();
+
       this.loading = false;
       this._changeDetectionRef.detectChanges();
     });
+  }
+
+  getDocuments() {
+    if (
+      !this.data ||
+      !this.data.documents ||
+      !this.data.documents.length ||
+      CommonUtils.isObject(this.data.documents[0])
+    ) {
+      return;
+    }
+
+    this.factoryService
+      .getDocuments(this.data.documents)
+      .pipe(takeUntil(this.ngUnsubscribe))
+      .subscribe((res: any) => {
+        if (!res || !res.length) {
+          return;
+        }
+
+        const documents = (res[0] && res[0].data && res[0].data.searchResults) || [];
+
+        this.data.documents = documents.map(document => {
+          return new Document(document);
+        });
+      });
   }
 
   activateTab(tabLabel: string): void {
