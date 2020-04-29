0e5f2c6a2bcf85bdf874f460847a353165fce326
Mark Lisé <marklise@users.noreply.github.com>
2020-04-29 16:43:55 -0700

    [PTI-214] Create/Edit News Items (#340)
    * [PTI-214] Create/Edit News Items
    
    * Removing test file for component that isn't running any real tests.

---
+++ api/src/controllers/put/news-item.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+let ObjectId = require('mongoose').Types.ObjectId;
+
+exports.editRecord = async function(args, res, next, incomingObj) {
+  try {
+    const Model = mongoose.model(incomingObj._schemaName);
+
+    // TODO: Something special for NRCED/BCMI?
+    if (incomingObj._schemaName === 'ActivityLNG') {
+      const record = await Model.findOneAndUpdate(
+          { _id: new ObjectId(incomingObj._id) },
+          { $set: {
+              projectName: incomingObj.projectName,
+              description: incomingObj.description,
+              url: incomingObj.url,
+              _epicProjectId: new ObjectId(incomingObj._epicProjectId),
+              type: incomingObj.type,
+              date: new Date(incomingObj.date)
+            }
+          },
+          { new: true }
+        );
+      return {
+        status: 'success',
+        object: record
+      };
+    }
+  } catch (e) {
+    return {
+      status: 'failure',
+      object: null,
+      errorMessage: e.message
+    };
+  }
+};
