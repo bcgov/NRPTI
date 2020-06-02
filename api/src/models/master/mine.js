8853d88fab46cc3783a691c59603baa622a97c9e
Max Wardle <max.wardle@gov.bc.ca>
2020-06-02 14:23:19 -0700

    NRPTI-86: initial Mine model commit (#379)
    * NRPTI-86: initial model commit
    
    * update based on comments
    
    * NRPT-86: update location to be geojson
    
    * NRPT-86: made bcmi model folder, and updated docs
    
    * NRPT-86: Added missing fields and documentation
    
    * rename mine model and typo

---
+++ api/src/models/master/mine.js
@@ -0,0 +1,54 @@
+let mongoose = require('mongoose');
+
+let GeoJSON = new mongoose.Schema(
+  {
+      type:
+      {
+        type: String,
+        enum: ['Point'],
+        required: true
+      },
+      coordinates:
+      {
+        type: [Number],
+        required: true
+      }
+  });
+
+module.exports = require('../../utils/model-schema-generator')(
+  'Mine',
+  {
+    _schemaName: { type: String, default: 'Mine', index: true },
+    _sourceRefId:  { type:'ObjectId', default: null, index: true },
+
+    read: [{ type: String, trim: true, default: 'sysadmin' }],
+    write: [{ type: String, trim: true, default: 'sysadmin' }],
+
+    name: { type: String, default: '' },
+    permitNumber: [{ type: String, default: '' }],
+    status: { type: String, default: '' },
+    type: { type: String, default: '' },
+    commodities: { type: String, default: '' },
+    tailingsImpoundments: { type: Number, default: 0 },
+    region: { type: String, default: '' },
+    location : { type: GeoJSON, default: { type: "Point", coordinates: [ 0.00, 0.00 ] } },
+    operator: { type: String, default: '' },
+    owner: { type: String, default: '' },
+    summary: { type: String, default: '' },
+    description: { type: String, default: '' },
+    links: [{ type: String, trim: true, default: '' }],
+
+    dateAdded: { type: Date, default: Date.now() },
+    dateUpdated: { type: Date, default: null },
+    datePublished: { type: Date, default: null },
+
+    addedBy: { type: String, default: '' },
+    updatedBy: { type: String, default: '' },
+    publishedBy: { type: String, default: '' },
+
+    sourceDateAdded: { type: Date, default: null },
+    sourceDateUpdated: { type: Date, default: null },
+    sourceSystemRef: { type: String, default: 'core' }
+  },
+  'nrpti'
+);
