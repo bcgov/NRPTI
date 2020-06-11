3df9cc937500b6693cf2497d54e9bdc31c06cec6
Nick Phura <Nick.Phura@quartech.com>
2020-06-11 14:07:06 -0700

    NRPT-71: Description & Summary keyword search. (#404)
    * NRPT-71: Description & Summary keyword search.
    - Add description & summary option to admin keyword search
    - Add description subset
    - Add new description summary subset text index migration
    
    * NRPT-71: Code review comments
    - add 'await's

--- api/materialized_views/updateViews.js
+++ api/materialized_views/updateViews.js
@@ -1,10 +1,11 @@
-
 const issedToSubset = require('./search/issuedToSubset');
 const locationSubset = require('./search/locationSubset');
+const descriptionSummarySubset = require('./search/descriptionSummarySubset');
 
 let updateAllMaterializedViews = async function (defaultLog) {
   await issedToSubset.update(defaultLog);
   await locationSubset.update(defaultLog);
+  await descriptionSummarySubset.update(defaultLog);
 };
 
 exports.updateAllMaterializedViews = updateAllMaterializedViews;
