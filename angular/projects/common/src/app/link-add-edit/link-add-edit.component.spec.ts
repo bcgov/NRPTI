0097cc4df063e62c1bd80615e410d412303baee0
Nick Phura <nickphura@gmail.com>
2020-06-22 19:43:15 -0700

    NRPT-159: fix failing unit test

--- angular/projects/common/src/app/link-add-edit/link-add-edit.component.spec.ts
+++ angular/projects/common/src/app/link-add-edit/link-add-edit.component.spec.ts
@@ -39,7 +39,7 @@ describe('LinkAddEditComponent', () => {
 
       expect(component.formArray.value.length).toEqual(2);
       expect({ ...component.formArray.value[0] }).toEqual({ title: 'titleA', url: 'urlA' });
-      expect({ ...component.formArray.value[1] }).toEqual({ title: null, url: null });
+      expect({ ...component.formArray.value[1] }).toEqual({ title: '', url: '' });
     });
   });
 
