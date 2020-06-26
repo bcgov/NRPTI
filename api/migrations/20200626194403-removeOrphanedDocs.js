3fb0c0014b44f40a4f53dd1c437c15124582029f
Daniel Truong <daniel@dtsoftware.ca>
2020-06-26 14:46:26 -0700

    Clean up

--- api/migrations/20200626194403-removeOrphanedDocs.js
+++ api/migrations/20200626194403-removeOrphanedDocs.js
@@ -1,10 +1,10 @@
 'use strict';
 
-var dbm;
-var type;
-var seed;
+let dbm;
+let type;
+let seed;
 
-var ObjectID = require('mongodb').ObjectID;
+let ObjectID = require('mongodb').ObjectID;
 
 /**
   * We receive the dbmigrate dependency from dbmigrate initially.
@@ -16,73 +16,71 @@ exports.setup = function (options, seedLink) {
   seed = seedLink;
 };
 
-exports.up = function (db) {
-  return db.connection.connect(db.connectionString, { native_parser: true })
-    .then(async (mClient) => {
-      var nrptiCollection = mClient.collection('nrpti');
-      console.log('Getting all records');
-      const records = await nrptiCollection.find(
-        {
-          _schemaName: {
-            $in: [
-              'AdministrativePenalty',
-              'AdministrativeSanction',
-              'Agreement',
-              'Certificate',
-              'ConstructionPlan',
-              'CourtConviction',
-              'Inspection',
-              'ManagementPlan',
-              'Order',
-              'Permit',
-              'RestorativeJustice',
-              'SelfReport',
-              'Ticket',
-              'Warning'
-            ]
-          }
-        }
-      ).toArray();
-      var recordDocIds = [];
-      for (let i = 0; i < records.length; i++) {
-        if (records[i].documents && records[i].documents.length > 0) {
-          records[i].documents.forEach(id => {
-            recordDocIds.push(ObjectID(id).toString());
-          });
+exports.up = async function (db) {
+  try {
+    let mClient = await db.connection.connect(db.connectionString, { native_parser: true });
+    let nrptiCollection = mClient.collection('nrpti');
+    console.log('Getting all records');
+    const records = await nrptiCollection.find(
+      {
+        _schemaName: {
+          $in: [
+            'AdministrativePenalty',
+            'AdministrativeSanction',
+            'Agreement',
+            'Certificate',
+            'ConstructionPlan',
+            'CourtConviction',
+            'Inspection',
+            'ManagementPlan',
+            'Order',
+            'Permit',
+            'RestorativeJustice',
+            'SelfReport',
+            'Ticket',
+            'Warning'
+          ]
         }
       }
-
-      console.log('Getting all documents');
-      const documents = await nrptiCollection.find(
-        {
-          _schemaName: 'Document'
-        }
-      ).toArray();
-
-      console.log('Finding orphaned documents');
-      var orphanedDocIds = [];
-      for (let j = 0; j < documents.length; j++) {
-        if (!recordDocIds.includes(ObjectID(documents[j]._id).toString())) {
-          orphanedDocIds.push(ObjectID(documents[j]._id));
+    ).toArray();
+    let recordDocIds = [];
+    for (let i = 0; i < records.length; i++) {
+      if (records[i].documents && records[i].documents.length > 0) {
+        for (let j = 0; j < records[i].documents.length; j++) {
+          recordDocIds.push(ObjectID(records[i].documents[j]).toString());
         }
       }
+    }
 
-      console.log('Deleted', orphanedDocIds.length, 'orphaned document(s)');
-      await nrptiCollection.deleteMany(
-        {
-          _id: {
-            $in: orphanedDocIds
-          }
+    console.log('Getting all documents');
+    const documents = await nrptiCollection.find(
+      {
+        _schemaName: 'Document'
+      }
+    ).toArray();
+
+    console.log('Finding orphaned documents');
+    let orphanedDocIds = [];
+    for (let k = 0; k < documents.length; k++) {
+      if (!recordDocIds.includes(ObjectID(documents[k]._id).toString())) {
+        orphanedDocIds.push(ObjectID(documents[k]._id));
+      }
+    }
+
+    console.log('Deleted', orphanedDocIds.length, 'orphaned document(s)');
+    await nrptiCollection.deleteMany(
+      {
+        _id: {
+          $in: orphanedDocIds
         }
-      );
+      }
+    );
 
-      mClient.close();
-    })
-    .catch((err) => {
-      console.log("Error on updating flavour publish statuses on masters: ", err);
-      mClient.close();
-    });
-};
+    mClient.close();
+  } catch (err) {
+    console.log("Error deleting orphans: ", err);
+  };
+}
 
 
 exports.down = function (db) {
