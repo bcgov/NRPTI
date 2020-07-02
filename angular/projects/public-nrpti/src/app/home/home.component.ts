e875dc1dfa39f4f9700bd6b379a279f8c2d16c5e
Dylan <dylan.hemsworth@gmail.com>
2020-07-02 16:47:51 -0700

    NRPT-226: Add more info paragraph, link (#469)
    * NRPT-226: Add more info paragraph, link
    
    * NRPT-226: Remove useless log

--- angular/projects/public-nrpti/src/app/home/home.component.ts
+++ angular/projects/public-nrpti/src/app/home/home.component.ts
@@ -1,12 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
-  constructor() {}
+export class HomeComponent implements OnInit, AfterViewInit {
+  constructor(public route: ActivatedRoute) {}
 
   ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.route.params
+    .subscribe((params) => {
+      // scroll to the div specified in the 'div' param
+      if (params && params.div) {
+        this.scrollTo(params.div);
+      }
+    });
+  }
+
+  scrollTo(div) {
+    const element = document.querySelector('#' + div);
+    if (element) {
+      element.scrollIntoView();
+    }
+  }
 }
