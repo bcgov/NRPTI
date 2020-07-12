f5171c02bb9252696092c8afcba437d833b75890
dhemsworth <dylan.hemsworth@gmail.com>
2020-07-11 22:07:07 -0700

    NRPT-199: Fix unit test

--- angular/projects/common/src/app/search-filter-template/search-filter-template.component.spec.ts
+++ angular/projects/common/src/app/search-filter-template/search-filter-template.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, TestBed } from '@angular/core/testing';
 import { ReactiveFormsModule } from '@angular/forms';
-import { MatAutocompleteModule, MatTooltip } from '@angular/material';
+import { MatAutocompleteModule, MatTooltipModule } from '@angular/material';
 import { TestBedHelper } from '../spec/spec-utils';
 import { SearchFilterTemplateComponent } from './search-filter-template.component';
 import { GlobalModule, Utils } from 'nrpti-angular-components';
@@ -10,7 +10,7 @@ describe('SearchFilterTemplateComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [ReactiveFormsModule, MatAutocompleteModule, GlobalModule, MatTooltip],
+      imports: [ReactiveFormsModule, MatAutocompleteModule, GlobalModule, MatTooltipModule],
       providers: [Utils],
       declarations: [SearchFilterTemplateComponent]
     }).compileComponents();
