84cb0221d017e45027a43a2434e6eb2c0e333209
dhemsworth <dylan.hemsworth@gmail.com>
2020-07-16 16:38:07 -0700

    NRPT-128: Set default sort to dateIssued

--- angular/projects/public-nrpti/src/app/records/records-list/records-list-resolver.ts
+++ angular/projects/public-nrpti/src/app/records/records-list/records-list-resolver.ts
@@ -63,7 +63,7 @@ export class RecordsListResolver implements Resolve<Observable<object>> {
       [],
       tableObject.currentPage,
       tableObject.pageSize,
-      tableObject.sortBy || '-dateAdded', // This needs to be common between all datasets to work properly
+      tableObject.sortBy || '-dateIssued', // This needs to be common between all datasets to work properly
       {},
       false,
       filterParams
