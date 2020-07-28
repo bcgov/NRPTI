8ce562404bbfba9c1c9e1900def4eb9b5e2f8fb2
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-07-28 15:13:07 -0700

    remove problem test

--- angular/projects/admin-nrpti/src/app/mines/mines-records-detail/mines-records-detail.component.spec.ts
+++ angular/projects/admin-nrpti/src/app/mines/mines-records-detail/mines-records-detail.component.spec.ts
@@ -1,33 +1,22 @@
 import { async, TestBed } from '@angular/core/testing';
-import { PermitDetailComponent } from './permit-detail.component';
-import { TestBedHelper, ActivatedRouteStub } from '../../../../../../common/src/app/spec/spec-utils';
+import { MatSlideToggleModule } from '@angular/material';
+import { MinesRecordDetailComponent } from './mines-records-detail.component';
+import { TestBedHelper } from '../../../../../common/src/app/spec/spec-utils'
 import { RouterTestingModule } from '@angular/router/testing';
-import { Router, ActivatedRoute } from '@angular/router';
 import { GlobalModule } from 'nrpti-angular-components';
-import { RecordDetailDirective } from '../../utils/record-detail.directive';
 import { DatePipe } from '@angular/common';
-import { CommonModule } from '../../../../../../common/src/app/common.module';
-import { FactoryService } from '../../../services/factory.service';
+import { CommonModule } from '../../../../../common/src/app/common.module';
 
-describe('PermitDetailComponent', () => {
-  const testBedHelper = new TestBedHelper<PermitDetailComponent>(PermitDetailComponent);
+describe('MinesRecordDetailComponent', () => {
+  const testBedHelper = new TestBedHelper<MinesRecordDetailComponent>(MinesRecordDetailComponent);
 
-  // component constructor mocks
-  const mockRouter = jasmine.createSpyObj('Router', ['navigate']);
-  const mockActivatedRoute = new ActivatedRouteStub();
-  const mockFactoryService = jasmine.createSpyObj('FactoryService', ['userInLngRole', 'userInNrcedRole']);
-  mockFactoryService.userInLngRole.and.returnValue(true);
-  mockFactoryService.userInNrcedRole.and.returnValue(true);
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      imports: [RouterTestingModule, GlobalModule, CommonModule],
-      declarations: [PermitDetailComponent, RecordDetailDirective],
+      imports: [RouterTestingModule, GlobalModule, CommonModule, MatSlideToggleModule],
+      declarations: [MinesRecordDetailComponent],
       providers: [
         DatePipe,
-        { provide: Router, useValue: mockRouter },
-        { provide: ActivatedRoute, useValue: mockActivatedRoute },
-        { provide: FactoryService, useValue: mockFactoryService }
       ]
     }).compileComponents();
   }));
