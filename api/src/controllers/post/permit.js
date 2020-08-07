9a5d48cddfe9cd8b92e6419a68bde6b0b638b826
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-08-07 13:34:43 -0700

    NRPT-276: Adds issued to to permit

--- api/src/controllers/post/permit.js
+++ api/src/controllers/post/permit.js
@@ -96,6 +96,7 @@ exports.createMaster = function (args, res, next, incomingObj, flavourIds) {
   incomingObj.recordSubtype && (permit.recordSubtype = incomingObj.recordSubtype);
   incomingObj.dateIssued && (permit.dateIssued = incomingObj.dateIssued);
   incomingObj.issuingAgency && (permit.issuingAgency = incomingObj.issuingAgency);
+  incomingObj.issuedTo && (permit.issuedTo = incomingObj.issuedTo);
   incomingObj.legislation && incomingObj.legislation.act && (permit.legislation.act = incomingObj.legislation.act);
   incomingObj.legislation &&
     incomingObj.legislation.regulation &&
