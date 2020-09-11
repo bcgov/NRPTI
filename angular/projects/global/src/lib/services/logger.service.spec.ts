f01ec81d307915f0f80473246c5f2024ceafce03
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-09-11 13:50:38 -0700

    Fix unit test

--- angular/projects/global/src/lib/services/logger.service.spec.ts
+++ angular/projects/global/src/lib/services/logger.service.spec.ts
@@ -1,5 +1,5 @@
 import { TestBed, inject } from '@angular/core/testing';
-import { LoggerService } from 'app/services/logger.service';
+import { LoggerService } from './logger.service';
 
 describe('LoggerService', () => {
   beforeEach(() => {
