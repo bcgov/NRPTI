2c5fd52f38cc64ae34d1c6299755a15db6f70cd9
Daniel Truong <daniel@dtsoftware.ca>
2020-10-28 11:44:32 -0700

    NRPT-499: Migration to remove duplicate certificate amendments

---
+++ api/migrations/20201028182858-removeCertAmendDups.js
@@ -0,0 +1,39 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function (options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function (db) {
+  console.log('**** Removing EPIC certificate amendments that are duplicates ****');
+  const mClient = await db.connection.connect(db.connectionString, { native_parser: true });
+
+  try {
+    const nrpti = await mClient.collection('nrpti');
+    await nrpti.deleteMany({ _sourceRefId: "", sourceSystemRef: 'epic' });
+  } catch (err) {
+    console.error('Error removing duplicates', err);
+  }
+
+  console.log('**** Duplicates removed ****');
+
+  mClient.close();
+}
+
+exports.down = function (db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
