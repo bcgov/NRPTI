593af9b8d911b0ca83dd4dbd3aa8fbe4fa7609f2
Kit Armstrong <kitarmstrongbc@gmail.com>
2020-11-18 16:49:36 -0800

    NRPT-525: Hide EMPR Permit Number (#691)
    * NRPT-525: Adds ability to display EMPR permit numbers

---
+++ api/migrations/202011181302125-addMinePermitFlag.js
@@ -0,0 +1,40 @@
+
+
+'use strict';
+
+let dbm;
+let type;
+let seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+}
+
+exports.up = async function(db) {
+  console.log('**** Adding permit flag to mines ****');
+  const mClient = await db.connection.connect(db.connectionString, { native_parser: true });
+
+  try {
+    const nrpti = await mClient.collection('nrpti');
+    await nrpti.updateMany({ _schemaName: 'MineBCMI' }, { $set: { showPermitNumber: true }});
+    console.log('**** Finished adding flag ****');
+  } catch (error) {
+    console.error(`Migration did not complete. Error processing records: ${error.message}`);
+  }
+
+  mClient.close();
+}
+
+exports.down = function(db) {
+  return null;
+}
+
+exports._meta = {
+  "version": 1
+}
