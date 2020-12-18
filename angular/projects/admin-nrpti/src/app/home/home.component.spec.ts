e3c13f8199ca8352a76979d24c9b6e825d482129
Morgan Thompson <morgan.thompson@gov.bc.ca>
2020-12-18 15:08:06 -0700

    disable record edit in table rows based on role, hide import card

--- angular/projects/admin-nrpti/src/app/home/home.component.spec.ts
+++ angular/projects/admin-nrpti/src/app/home/home.component.spec.ts
@@ -4,6 +4,8 @@ import { HomeComponent } from './home.component';
 import { RouterTestingModule } from '@angular/router/testing';
 
 import { LoadingScreenService } from 'nrpti-angular-components';
+import { Constants } from '../utils/constants/misc';
+import { KeycloakService } from '../services/keycloak.service';
 
 describe('HomeComponent', () => {
   let component: HomeComponent;
@@ -18,13 +20,31 @@ describe('HomeComponent', () => {
     }
   };
 
+  const mockKeyCloakService = {
+    isMenuEnabled: (menuName) => {
+      let retVal = false;
+      switch (menuName) {
+        case Constants.Menus.ALL_MINES: retVal = true; break;
+        case Constants.Menus.ALL_RECORDS: retVal = true; break;
+        case Constants.Menus.NEWS_LIST: retVal = true; break;
+        case Constants.Menus.ANALYTICS: retVal = false; break;
+        case Constants.Menus.MAP: retVal = false; break;
+        case Constants.Menus.ENTITIES: retVal = false; break;
+        case Constants.Menus.IMPORTS: retVal = true; break;
+        case Constants.Menus.COMMUNICATIONS: retVal = true; break;
+      }
+      return retVal;
+    }
+  };
+
   beforeEach((() => {
     TestBed.configureTestingModule({
       declarations: [HomeComponent],
       imports: [RouterTestingModule],
       providers: [
         { provide: 'Router', useValue: mockRouter },
-        { provide: LoadingScreenService, useValue: mockLoadingScreenService }
+        { provide: LoadingScreenService, useValue: mockLoadingScreenService },
+        { provide: KeycloakService, useValue: mockKeyCloakService }
       ]
     }).compileComponents();
   }));
