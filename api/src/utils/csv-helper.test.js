1c0bb798470614a38977c51a134be4e4ad49273d
Morgan Thompson <morgan.thompson@gov.bc.ca>
2021-04-23 11:54:10 -0700

    add unit test for csv-helper

--- api/src/utils/csv-helper.test.js
+++ api/src/utils/csv-helper.test.js
@@ -1,23 +1,21 @@
-const AWS = require('aws-sdk');
 const awsMock = require('aws-sdk-mock');
-const Readable = require('stream').Readable;
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
 
+// Mock has to be setup before csv-helpers is loaded or the S3 connection doesn't use the mock
+awsMock.setSDKInstance(AWS);
+awsMock.mock('S3', 'getObject', Buffer.from(fs.readFileSync(path.resolve(__dirname, '../tests/fixture/mockCsv.csv'))));
 const { readAndParseCsvFile } = require('./csv-helpers');
 
-const csvBody = 'Record ID,Date,Compliance Status,Region,Function,Activity,Parent Act,Act or Regulation,Client Type,Client / Complainant,Latitude,Longitude,Action Taken,Client No,Report Status,Section \
-999999,2021-02-24,Alleged Thing,Somewhere,Grass Management,Mowing,Lawn Care,Lawn Care Act,C,A Copmany,54.432,-124.5,Cutting Action,111111,Complete,"Mowing without a license"';
-
-AWS.config.paramValidation = false;
-
 describe('readAndParseCsvFile', () => {
-  it('returns null if csv stream is empty ', async () => {
-    const bodyStream = new Readable();
-    bodyStream.push(csvBody);
-    bodyStream.push(null);
-    awsMock.mock('S3', 'getObject', bodyStream)
+  it('returns parsed row if csv is valid ', async () => {
     const stream = new AWS.S3().getObject('getObject').createReadStream();
     const result = await readAndParseCsvFile(stream, 'nris-flnr-csv', 'Inspection');
-
-    expect(result).toBe(null);
+    expect(result).t
+    expect(result).toHaveLength(1);
+    expect(result[0]['section']).toBe('Mowing without a license');
+    expect(result[0]['date']).toBe('2021-02-24');
+    expect(result[0]['record id']).toBe('999999');
   });
 })
