c837de85de023cf69f25ad47e3dedf870e8fc92f
Morgan Thompson <morgan.thompson@gov.bc.ca>
2021-04-27 13:37:54 -0700

    NRPT-733: import FLNRO inspections from FME csv output (#837)
    * NRPT-733: import FLNRO inspections from FME csv output
    
    * apply sonar fixes
    
    * fix cron import target, unnecessary async
    
    * add unit test for csv-helper
    
    * fix missing test expect
    
    * add await for readAndParseCsvFile

---
+++ api/src/utils/csv-helper.test.js
@@ -0,0 +1,21 @@
+const awsMock = require('aws-sdk-mock');
+const AWS = require('aws-sdk');
+const fs = require('fs');
+const path = require('path');
+
+// Mock has to be setup before csv-helpers is loaded or the S3 connection doesn't use the mock
+awsMock.setSDKInstance(AWS);
+awsMock.mock('S3', 'getObject', Buffer.from(fs.readFileSync(path.resolve(__dirname, '../tests/fixture/mockCsv.csv'))));
+const { readAndParseCsvFile } = require('./csv-helpers');
+
+describe('readAndParseCsvFile', () => {
+  it('returns parsed row if csv is valid ', async () => {
+    const stream = new AWS.S3().getObject('getObject').createReadStream();
+    const result = await readAndParseCsvFile(stream, 'nris-flnr-csv', 'Inspection');
+    expect(result).toBeDefined();
+    expect(result).toHaveLength(1);
+    expect(result[0]['section']).toBe('Mowing without a license');
+    expect(result[0]['date']).toBe('2021-02-24');
+    expect(result[0]['record id']).toBe('999999');
+  });
+})
