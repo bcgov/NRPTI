878885a436bbc0bbf535f1dc508f93edd1fb468b
BcGovNeal <47604552+BcGovNeal@users.noreply.github.com>
2021-07-08 15:13:37 -0700

    Enforcement actions fixes

--- angular/projects/admin-nrpti/src/app/enforcement-actions/enforcement-actions-table/enforcement-actions-table-row/enforcement-actions-table-row.component.ts
+++ angular/projects/admin-nrpti/src/app/enforcement-actions/enforcement-actions-table/enforcement-actions-table-row/enforcement-actions-table-row.component.ts
@@ -1,25 +1,34 @@
-import { Component, HostListener, OnInit, ChangeDetectorRef } from '@angular/core';
+import { Component, HostListener, OnInit, OnDestroy, ChangeDetectorRef } from '@angular/core';
+import { takeUntil, catchError } from 'rxjs/operators';
 import { Router } from '@angular/router';
+import { Subject, of } from 'rxjs';
+
 import { TableRowComponent } from 'nrpti-angular-components';
 import { FactoryService } from '../../../services/factory.service';
+import { LoggerService } from 'nrpti-angular-components';
 
 @Component({
   selector: 'tr[app-enforcement-actions-table-row]',
   templateUrl: './enforcement-actions-table-row.component.html',
   styleUrls: ['./enforcement-actions-table-row.component.scss']
 })
-export class EnforcementActionsTableRowComponent extends TableRowComponent implements OnInit {
+export class EnforcementActionsTableRowComponent extends TableRowComponent implements OnInit, OnDestroy {
+  private ngUnsubscribe: Subject<boolean> = new Subject<boolean>();
+
+  private bcmiFlavour: any;
   public isPublished = false;
-  public factoryService: FactoryService;
 
   constructor(
     private router: Router,
+    public factoryService: FactoryService,
+    private logger: LoggerService,
     public changeDetectionRef: ChangeDetectorRef
   ) {
     super();
   }
 
   ngOnInit() {
+    this.bcmiFlavour = this.rowData.flavours.find(flavour => flavour._schemaName.endsWith('BCMI'));
     this.isPublished = this.isRecordPublished();
     this.changeDetectionRef.detectChanges();
   }
@@ -37,30 +46,96 @@ export class EnforcementActionsTableRowComponent extends TableRowComponent imple
     }
   }
   goToDetails() {
-    this.router.navigate(
-      ['mines', 'enforcement-actions', this.getSchemaRoute(this.rowData._schemaName), this.rowData._id]
-    );
+    this.router.navigate([
+      'mines',
+      'enforcement-actions',
+      this.getSchemaRoute(this.rowData._schemaName),
+      this.rowData._id
+    ]);
   }
 
   goToEdit() {
-    this.router.navigate(
-      ['mines', 'enforcement-actions', this.getSchemaRoute(this.rowData._schemaName), this.rowData._id, 'edit']
-    );
+    this.router.navigate([
+      'mines',
+      'enforcement-actions',
+      this.getSchemaRoute(this.rowData._schemaName),
+      this.rowData._id,
+      'edit'
+    ]);
   }
 
   publish() {
-    // to be implemented
+    if (!this.bcmiFlavour) {
+      alert('Failed to unpublish record. No BCMI record found.');
+      return;
+    }
+
+    this.factoryService
+      .publishRecord(this.bcmiFlavour)
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        catchError(error => {
+          this.logger.log(`Publish error: ${error}`);
+          alert('Failed to publish record.');
+          return of(null);
+        })
+      )
+      .subscribe(response => {
+        if (!response) {
+          return;
+        }
+
+        if (response.code === 409) {
+          // object was already published
+          return;
+        }
+
+        this.isPublished = true;
+        this.changeDetectionRef.detectChanges();
+      });
   }
 
   unPublish() {
-    // to be implemented
+    if (!this.bcmiFlavour) {
+      alert('Failed to unpublish record. No BCMI record found.');
+      return;
+    }
+
+    this.factoryService
+      .unPublishRecord(this.bcmiFlavour)
+      .pipe(
+        takeUntil(this.ngUnsubscribe),
+        catchError(error => {
+          this.logger.log(`Unpublish error: ${error}`);
+          alert('Failed to unpublish record.');
+          return of(null);
+        })
+      )
+      .subscribe(response => {
+        if (!response) {
+          return;
+        }
+
+        if (response.code === 409) {
+          // object was already unpublished
+          return;
+        }
+
+        this.isPublished = false;
+        this.changeDetectionRef.detectChanges();
+      });
   }
 
   isRecordPublished(): boolean {
-    return this.rowData && this.rowData.read && this.rowData.read.includes('public');
+    return this.rowData && this.rowData.read && this.rowData.isBcmiPublished;
   }
 
   @HostListener('click') onItemClicked() {
     this.goToDetails();
+  }
+
+  ngOnDestroy() {
+    this.ngUnsubscribe.next();
+    this.ngUnsubscribe.complete();
   }
 }
