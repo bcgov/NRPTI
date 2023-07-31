07b6d1c0406f32d5d2b86d5a4cb568135f9f488f
David <62899351+davidclaveau@users.noreply.github.com>
2023-07-31 14:24:05 -0700

    update migration to look for dob

--- api/migrations/20230727000836-unredactMinistryOfForests.js
+++ api/migrations/20230727000836-unredactMinistryOfForests.js
@@ -36,21 +36,27 @@ exports.up = async function(db) {
       })
       .toArray();
 
-      const promises = records.map(async (record) => {
-        if (
-          record.issuedTo &&
-          record.issuedTo.type === 'Individual'
-        ) {
+    const promises = records.map(async (record) => {
+      if (
+        record.issuedTo &&
+        record.issuedTo.type === 'Individual' &&
+        record.issuedTo.dateOfBirth
+      ) {
+        const dateOfBirth = new Date(record.issuedTo.dateOfBirth);
+        const today = new Date();
+        const ageInYears = today.getFullYear() - dateOfBirth.getFullYear();
+    
+        if (ageInYears >= 19) {
           // Save the "issuedTo" object in the "nrpti" collection
           // Add "public" to the "read" array in the "issuedTo" object
           await nrpti.updateOne({ _id: record._id }, { $set: { issuedTo: record.issuedTo } });
           const issuedToWithPublic = { ...record.issuedTo, read: [...record.issuedTo.read, 'public'] };
-      
+    
           // Make sure the "_flavourRecords" ID exists on the master record
           // Check if the corresponding "_flavourRecords" ID exists in the "redacted_record_subset" collection
           if (record._flavourRecords && record._flavourRecords.length > 0) {
             const redactedRecordSubsetDocument = await redactedRecordSubset.findOne({ _id: record._flavourRecords[0] });
-      
+    
             // Update the "issuedTo" object in the "redacted_record_subset" collection
             // Otherwise, insert the "issuedTo" data in the "redacted_record_subset" collection
             if (redactedRecordSubsetDocument) {
@@ -64,8 +70,19 @@ exports.up = async function(db) {
           } else {
             console.error(`Skipping record ${record._id} because _flavourRecords is undefined or empty.`);
           }
+        } else {
+          // Remove issuedTo from redacted_record_subset if the individual is younger than 19 years old
+          if (record._flavourRecords && record._flavourRecords.length > 0) {
+            await redactedRecordSubset.updateOne(
+              { _id: record._flavourRecords[0] },
+              { $unset: { issuedTo: 1 } }
+            );
+          }
+          console.log(`Removing issuedTo for record ${record._id} because the individual is younger than 19 years old.`);
         }
-      });
+      }
+    });
+      
 
     await Promise.all(promises);
 
