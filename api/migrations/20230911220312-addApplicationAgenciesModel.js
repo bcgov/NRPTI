7e356ceb1cd408150c1d3f3872363aaf0aa89ca5
David <62899351+davidclaveau@users.noreply.github.com>
2023-09-12 12:22:38 -0700

    update model to only add to nrpti collection

--- api/migrations/20230911220312-addApplicationAgenciesModel.js
+++ api/migrations/20230911220312-addApplicationAgenciesModel.js
@@ -16,58 +16,62 @@ exports.up = async function (db) {
   console.log('**** Adding ApplicationAgencies constants to nrpti collection ****');
   
   // Connect to the database
-  mongoose.connect(db.connectionString, { useNewUrlParser: true, useUnifiedTopology: true });
-  const dbConnection = mongoose.connection;
-  
-  dbConnection.on('error', console.error.bind(console, 'MongoDB connection error:'));
-  dbConnection.once('open', async function () {
-    const agencies = {
-      AGENCY_ALC: 'Agricultural Land Commission',
-      AGENCY_WF: 'BC Wildfire Service',
-      AGENCY_ENV_COS: 'Conservation Officer Service',
-      AGENCY_EAO: 'Environmental Assessment Office',
-      AGENCY_EMLI: 'Ministry of Energy Mines and Low Carbon Innovation',
-      AGENCY_ENV: 'Ministry of Environment and Climate Change Strategy',
-      AGENCY_ENV_BCPARKS: 'BC Parks',
-      AGENCY_OGC: 'BC Energy Regulator',
-      AGENCY_ENV_EPD: 'Ministry of Environment and Climate Change Strategy',
-      AGENCY_LNG: 'LNG Secretariat',
-      AGENCY_AGRI: 'Ministry of Agriculture and Food',
-      AGENCY_FLNRO: 'Ministry of Forests',
-      AGENCY_FLNR_NRO: 'Natural Resource Officers',
-      AGENCY_WLRS: 'Ministry of Water, Land and Resource Stewardship',
-    };
-  
-    // Create and insert documents for each agency
-    const agencyInsertPromises = Object.entries(agencies).map(([code, name]) =>
-      ApplicationAgency.create(
-        { 
-          agencyCode: code,
-          agencyName: name,
-          read: ['sysadmin'],
-          write: ['sysadmin'],
-         }
-        )
-        .catch(error => {
-          console.error(`Error inserting agency ${code}:`, error);
-        })
-    );
-  
-    try {
-      await Promise.all(agencyInsertPromises);
-      console.log('Migration completed successfully');
-    } catch (error) {
-      console.error('Migration failed:', error);
-    } finally {
-      mongoose.connection.close(() => {
-        mongoose.connection.close();
-        console.log('Database connection closed');
-        process.exit(0);
-      });
-
-      return null
-    }
+  const mClient = await db.connection.connect(db.connectionString, {
+    native_parser: true,
   });
+  
+  const agencies = [
+    { agencyCode: "AGENCY_ALC", agencyName: 'Agricultural Land Commission' },
+    { agencyCode: "AGENCY_WF", agencyName: 'BC Wildfire Service' },
+    { agencyCode: "AGENCY_ENV_COS", agencyName: 'Conservation Officer Service' },
+    { agencyCode: "AGENCY_EAO", agencyName: 'Environmental Assessment Office' },
+    { agencyCode: "AGENCY_EMLI", agencyName: 'Ministry of Energy Mines and Low Carbon Innovation' },
+    { agencyCode: "AGENCY_ENV", agencyName: 'Ministry of Environment and Climate Change Strategy' },
+    { agencyCode: "AGENCY_ENV_BCPARKS", agencyName: 'BC Parks' },
+    { agencyCode: "AGENCY_OGC", agencyName: 'BC Energy Regulator' },
+    { agencyCode: "AGENCY_ENV_EPD", agencyName: 'Ministry of Environment and Climate Change Strategy' },
+    { agencyCode: "AGENCY_LNG", agencyName: 'LNG Secretariat' },
+    { agencyCode: "AGENCY_AGRI", agencyName: 'Ministry of Agriculture and Food' },
+    { agencyCode: "AGENCY_FLNRO", agencyName: 'Ministry of Forests' },
+    { agencyCode: "AGENCY_FLNR_NRO", agencyName: 'Natural Resource Officers' },
+    { agencyCode: "AGENCY_WLRS", agencyName: 'Ministry of Water, Land and Resource Stewardship' },
+  ]
+
+  try {
+    let currentCollection = await mClient.collection('nrpti');
+    
+    for (const agency of agencies) {
+      const existingAgency = await currentCollection.findOne({ _schemaName: 'ApplicationAgency', agencyCode: agency['agencyCode'] });
+
+      if (!existingAgency) {
+        await currentCollection.insertOne(
+          { 
+            _schemaName: 'ApplicationAgency',
+            agencyCode: agency['agencyCode'],
+            agencyName: agency['agencyName'],
+            read: ['sysadmin'],
+            write: ['sysadmin'],
+            dateAdded: new Date(),
+            dateUpdated: null,
+            addedBy: '',
+            updatedBy: '',
+          }
+        );
+          console.log(` **** Add the ApplicationAgency code ${agency['agencyCode']} into nrpti collection ****`);
+      } else {
+        console.log(' **** ApplicationAgency code already exists in nrpti collection ****')
+      }
+    }
+  } catch (err) {
+    console.log(` **** Error updating nrpti collection for agency: ${agency['agencyName']} ****`, err);
+  } finally {
+    if (mClient) {
+      console.log(' **** Closing connection to nrpti collection ****')
+      await mClient.close();
+    }
+  }
+
+  return null;
 };
 
 exports.down = function(db) {
