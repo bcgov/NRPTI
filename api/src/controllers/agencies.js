8fe57851f02d2b61854a9ef0ae6832446a504c41
Sanjay Babu <sanjaytkbabu@gmail.com>
2023-09-13 14:02:21 -0700

    put api added for updating agencies

--- api/src/controllers/agencies.js
+++ api/src/controllers/agencies.js
@@ -1,82 +1,75 @@
-const QueryActions = require('../utils/query-actions');
+const queryActions = require('../utils/query-actions');
 const mongodb = require('../utils/mongodb');
 const RECORD_TYPE = require('../utils/constants/record-type-enum');
+const defaultLog = require('../utils/logger')('record');
 
-exports.publicGet = async function (args, res, next) {
-
-const db = mongodb.connection.db(process.env.MONGODB_DATABASE || 'nrpti-dev');
-const collectionDB = db.collection('nrpti');
+exports.publicGet = async function(args, res, next) {
+  const db = mongodb.connection.db(process.env.MONGODB_DATABASE || 'nrpti-dev');
+  const collectionDB = db.collection('nrpti');
 
   let agencyList;
 
   try {
     //Obtain documents with Application Agency Schema
-     let agencyDocuments = await collectionDB.find({_schemaName: RECORD_TYPE.ApplicationAgency._schemaName}).toArray();
-    //Using map function to iterate through the original array and creates a new array with objects containing only the _id, agencyCode, and agencyName properties. 
-     agencyList = agencyDocuments.map(item => ({
+    let agencyDocuments = await collectionDB.find({ _schemaName: RECORD_TYPE.ApplicationAgency._schemaName }).toArray();
+    //Using map function to iterate through the original array and creates a new array with objects containing only the _id, agencyCode, and agencyName properties.
+    agencyList = agencyDocuments.map(item => ({
       _id: item._id,
       agencyCode: item.agencyCode,
       agencyName: item.agencyName
     }));
-    console.log('hello from publicGet')
   } catch (error) {
-    console.log(error)
+    defaultLog.log(error);
     throw error;
   }
 
-  QueryActions.sendResponse(res, 200, agencyList);
+  queryActions.sendResponse(res, 200, agencyList);
 };
+/**
+* @param {*} args
+* @param {*} res
+* @param {*} next
+* @param {*} incomingObj see example
+* @returns edited lng order record
+* Example of args.swagger.params.data.value
+ {
+     "agencies":
+     [
+         {
+         "agencyCode": "AGENCY_ALC",
+         "agencyName": "Agricultural Land Commission_1"
+         }
+     ]
+ }
+*/
 
-// exports.protectedPost = async function (args, res, next) {
-//   // Confirm user has correct role for this type of record.
-//   // if (!userHasValidRoles([utils.ApplicationRoles.ADMIN, utils.ApplicationRoles.ADMIN_BCMI], args.swagger.params.auth_payload.client_roles)) {
-//   //   throw new Error('Missing valid user role.');
-//   // }
-
-//   // let incomingObj = {};
-//   // if (args.swagger.params.collection && args.swagger.params.collection.value) {
-//   //   incomingObj = args.swagger.params.collection.value
-//   // } else {
-//   //   defaultLog.info(`protectedPost - you must provide an id to post`);
-//   //   queryActions.sendResponse(res, 400, {});
-//   //   next();
-//   // }
-
-//   // let obj = null;
-//   // try {
-//   //   // Need to determine the published state of the mine and make the new collection match.
-//   //   const minePublished = await isMinePublished(incomingObj);
-//   //   if (minePublished) {
-//   //     incomingObj.addRole = 'public';
-//   //   }
-
-//   //   obj = await createCollection(incomingObj, args.swagger.params.auth_payload.displayName);
-//   // } catch (error) {
-//   //   defaultLog.info(`protectedPost - error inserting collection: ${incomingObj}`);
-//   //   defaultLog.debug(error);
-//   //   return queryActions.sendResponse(res, 400, {});
-//   // }
-//   let promises = [];
-//   const collectionDB = db.collection('nrpti');
-//     promises.push(collectionDB.findOneAndUpdate(
-//       { agencyCode: args.agencyCode },
-//       {
-//         $set: {
-//           agencyName: args.agencyName
-//         },
-//         $pull: { read: 'public' }
-//       }
-//     ));
-
-
-//   try {
-//     await Promise.all(promises);
-//   } catch (error) {
-//     defaultLog.info(`protectedPut - collection controller - error updating records with: ${collectionId}`);
-//     defaultLog.debug(error);
-//     throw error;
-//   }
-
-//   queryActions.sendResponse(res, 200, obj.ops[0]);
-//   next();
-// }
+exports.protectedPut = async function(args, res, next) {
+  const db = mongodb.connection.db(process.env.MONGODB_DATABASE || 'nrpti-dev');
+  let promises = [];
+  let result = null;
+  let incomingObj = args.swagger.params.data.value;
+  const collectionDB = db.collection('nrpti');
+  for (let agency of incomingObj['agencies']) {
+    promises.push(
+      collectionDB.findOneAndUpdate(
+        { agencyCode: agency['agencyCode'] },
+        {
+          $set: {
+            agencyName: agency['agencyName']
+          }
+        }
+      )
+    );
+  }
+  try {
+    await Promise.all(promises);
+    result = 'Success';
+  } catch (error) {
+    defaultLog.info(`protectedPut - agencies controller - error updating record: ${incomingObj}`);
+    defaultLog.debug(error);
+    result = 'Error';
+    return queryActions.sendResponse(res, 400, {});
+  }
+  queryActions.sendResponse(res, 200, result);
+  next();
+};
