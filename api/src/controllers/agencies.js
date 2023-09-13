23044bbcf46ae482261cc7f63a8b9e74ed2b2866
Sanjay Babu <sanjaytkbabu@gmail.com>
2023-09-13 10:22:15 -0700

    added get api

--- api/src/controllers/agencies.js
+++ api/src/controllers/agencies.js
@@ -2,7 +2,7 @@ const QueryActions = require('../utils/query-actions');
 const mongodb = require('../utils/mongodb');
 const RECORD_TYPE = require('../utils/constants/record-type-enum');
 
-exports.publicGetConfig = async function (args, res, next) {
+exports.publicGet = async function (args, res, next) {
 
 const db = mongodb.connection.db(process.env.MONGODB_DATABASE || 'nrpti-dev');
 const collectionDB = db.collection('nrpti');
@@ -18,6 +18,7 @@ const collectionDB = db.collection('nrpti');
       agencyCode: item.agencyCode,
       agencyName: item.agencyName
     }));
+    console.log('hello from publicGet')
   } catch (error) {
     console.log(error)
     throw error;
@@ -25,3 +26,57 @@ const collectionDB = db.collection('nrpti');
 
   QueryActions.sendResponse(res, 200, agencyList);
 };
+
+// exports.protectedPost = async function (args, res, next) {
+//   // Confirm user has correct role for this type of record.
+//   // if (!userHasValidRoles([utils.ApplicationRoles.ADMIN, utils.ApplicationRoles.ADMIN_BCMI], args.swagger.params.auth_payload.client_roles)) {
+//   //   throw new Error('Missing valid user role.');
+//   // }
+
+//   // let incomingObj = {};
+//   // if (args.swagger.params.collection && args.swagger.params.collection.value) {
+//   //   incomingObj = args.swagger.params.collection.value
+//   // } else {
+//   //   defaultLog.info(`protectedPost - you must provide an id to post`);
+//   //   queryActions.sendResponse(res, 400, {});
+//   //   next();
+//   // }
+
+//   // let obj = null;
+//   // try {
+//   //   // Need to determine the published state of the mine and make the new collection match.
+//   //   const minePublished = await isMinePublished(incomingObj);
+//   //   if (minePublished) {
+//   //     incomingObj.addRole = 'public';
+//   //   }
+
+//   //   obj = await createCollection(incomingObj, args.swagger.params.auth_payload.displayName);
+//   // } catch (error) {
+//   //   defaultLog.info(`protectedPost - error inserting collection: ${incomingObj}`);
+//   //   defaultLog.debug(error);
+//   //   return queryActions.sendResponse(res, 400, {});
+//   // }
+//   let promises = [];
+//   const collectionDB = db.collection('nrpti');
+//     promises.push(collectionDB.findOneAndUpdate(
+//       { agencyCode: args.agencyCode },
+//       {
+//         $set: {
+//           agencyName: args.agencyName
+//         },
+//         $pull: { read: 'public' }
+//       }
+//     ));
+
+
+//   try {
+//     await Promise.all(promises);
+//   } catch (error) {
+//     defaultLog.info(`protectedPut - collection controller - error updating records with: ${collectionId}`);
+//     defaultLog.debug(error);
+//     throw error;
+//   }
+
+//   queryActions.sendResponse(res, 200, obj.ops[0]);
+//   next();
+// }
