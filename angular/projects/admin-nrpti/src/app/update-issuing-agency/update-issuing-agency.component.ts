1b8b7b0246c06f12bf08254ebbba80d90f519fb6
Christopher-walsh22 <106549296+Christopher-walsh22@users.noreply.github.com>
2023-09-14 09:58:24 -0700

    Fixed component to properly display the information returned from get request

--- angular/projects/admin-nrpti/src/app/update-issuing-agency/update-issuing-agency.component.ts
+++ angular/projects/admin-nrpti/src/app/update-issuing-agency/update-issuing-agency.component.ts
@@ -13,7 +13,8 @@ export class UpdateIssuingAgencyComponent implements OnInit {
   selectedAgency: string = ''; // Initialize the selectedAgency
   choiceMade = false;
   newAgency: string = ''; // Initialize the new agency input field
-  agencies$: Observable<{ [key: string]: string }> = of({"Kyle": "Williams"}); // Initialize agencies as an Observable
+  agencies: { [key: string]: string } = {"Kyle": "Williams"};
+  agencyList = []
 
   constructor(
     private issuingAgencyService: IssuingAgencyService,
@@ -27,15 +28,15 @@ export class UpdateIssuingAgencyComponent implements OnInit {
 
   updateSelectedAgency(): void {
     if (this.newAgency.trim() !== '') {
-      // Only update the selected agency if the new agency input is not empty
-      this.agencies$.subscribe(agencies => {
-        agencies[this.selectedAgency] = this.newAgency;
+
+        this.agencies[this.selectedAgency] = this.newAgency;
         this.selectedAgency = this.newAgency; // Update the selected value
         this.newAgency = ''; // Clear the input field
         this.choiceMade = true;
-      });
+      };
+      alert(this.agencies)
     }
-  }
+
 
   ngOnInit(): void {
     this.issuingAgencyService.getIssuingAgencies()
@@ -46,9 +47,17 @@ export class UpdateIssuingAgencyComponent implements OnInit {
             agencies[agency._id] = agency.agencyName;
           });
         }
-        this.agencies$ = of(agencies); // Assign the agencies object as an Observable
+        this.agencies = agencies; // Assign the agencies object as an Observable
         console.log("IS IT IN THE CONSOLE?")
-        alert(JSON.stringify(this.agencies$))
+        alert(JSON.stringify(this.agencies))
+
+        for (const key in agencies) {
+          if (agencies.hasOwnProperty(key)) {
+            this.agencyList.push(agencies[key]);
+          }
+        }
+        alert(this.agencyList)
+
       })
       .catch(error => {
         console.error('API call error:', error);
