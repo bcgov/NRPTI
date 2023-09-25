370fcee2afb1ac5008d7d5b31805e3c351023be9
David <62899351+davidclaveau@users.noreply.github.com>
2023-09-25 14:23:50 -0700

    remove async ngOnInit, no longer needed

--- angular/projects/admin-nrpti/src/app/records/administrative-penalties/administrative-penalty-add-edit/administrative-penalty-add-edit.component.ts
+++ angular/projects/admin-nrpti/src/app/records/administrative-penalties/administrative-penalty-add-edit/administrative-penalty-add-edit.component.ts
@@ -61,7 +61,7 @@ export class AdministrativePenaltyAddEditComponent implements OnInit, OnDestroy 
     protected storeService: StoreService,
   ) { }
 
-  async ngOnInit() {
+  ngOnInit() {
     this.route.data.pipe(takeUntil(this.ngUnsubscribe)).subscribe((res: any) => {
       this.isEditing = res.breadcrumb !== 'Add Administrative Penalty';
       if (this.isEditing) {
@@ -85,20 +85,6 @@ export class AdministrativePenaltyAddEditComponent implements OnInit, OnDestroy 
 
       this.loading = false;
       this._changeDetectionRef.detectChanges();
-
-      this.route.data.pipe(takeUntil(this.ngUnsubscribe)).subscribe(async (res: any) => {
-        this.agencies = await this.fetchAgencies();
-        this.buildForm();
-      });
-    });
-  }
-
-  private async fetchAgencies(): Promise<any[]> {
-    return new Promise<any[]>((resolve) => {
-      setTimeout(() => {
-        const agenciesData = Picklists.getAgencyNames(this.factoryService);
-        resolve(agenciesData);
-      }, 1000);
     });
   }
 
