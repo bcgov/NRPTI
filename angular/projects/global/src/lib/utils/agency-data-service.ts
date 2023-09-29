0aeedfbdbc526f2364683e15486e4234491316f9
Sanjay Babu <sanjaytkbabu@gmail.com>
2023-09-29 09:13:32 -0700

    API unit tests fix

--- angular/projects/global/src/lib/utils/agency-data-service.ts
+++ angular/projects/global/src/lib/utils/agency-data-service.ts
@@ -7,26 +7,39 @@ export class AgencyDataService {
 
   displayNameFull(agencyCode): string {
     // Access cached agency data from FactoryService
-    const agencyList = this.factoryService.applicationAgencyService.getAgencies();
-    return agencyList[agencyCode] || agencyCode;
+    const agencyService = this.factoryService.applicationAgencyService;
+    const agencyList = agencyService ? agencyService.getAgencies() : null;
+
+    return agencyList && agencyList[agencyCode] ? agencyList[agencyCode] : agencyCode;
   }
 
   getAgencyCodes(): string[] {
-    const agencyList = this.factoryService.applicationAgencyService.getAgencies();
-    return Object.keys(agencyList);
+    const agencyService = this.factoryService.applicationAgencyService;
+    const agencyList = agencyService ? agencyService.getAgencies() : null;
+
+    return agencyList ? Object.keys(agencyList) : [];
   }
 
   getAgencyNames(): string[] {
-    const agencyList = this.factoryService.applicationAgencyService.getAgencies();
-    return Object.values(agencyList);
+    const agencyService = this.factoryService.applicationAgencyService;
+    const agencyList = agencyService ? agencyService.getAgencies() : null;
+
+    return agencyList ? Object.values(agencyList) : [];
   }
 
   getAgencyCode(agencyName: string): string {
-    const agencyList = this.factoryService.applicationAgencyService.getAgencies();
-    for (const key in agencyList) {
-      if (agencyList.hasOwnProperty(key) && agencyList[key] === agencyName) {
-        return key; // Return the key if the value matches
+    const agencyService = this.factoryService.applicationAgencyService;
+    const agencyList = agencyService ? agencyService.getAgencies() : null;
+
+    if (agencyList) {
+      for (const key in agencyList) {
+        if (agencyList.hasOwnProperty(key) && agencyList[key] === agencyName) {
+          return key; // Return the key if the value matches
+        }
       }
     }
+    // Return a default value or handle the case where agencyList is undefined or empty
+    return '';
   }
+
 }
