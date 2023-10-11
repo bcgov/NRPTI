5f1507009dbefd0a0f50be242d2ce03ae6cbd7de
Kane Smith <kane.smith@gov.bc.ca>
2023-10-11 14:57:45 -0700

    [NRPTI-1112] Add outcome description values to CSV outputs in NRCED

---
+++ angular/projects/public-nrpti/src/app/records/utils/record-utils.spec.ts
@@ -0,0 +1,56 @@
+import { RecordUtils } from './record-utils';
+import moment from 'moment';
+
+const mockData = [
+  {
+    recordType: 'testRecord',
+    dateIssued: '2023-10-11',
+    issuedTo: {
+      type: 'Company',
+      companyName: 'This company doesnt exist'
+    },
+    summary: 'In summary, this is a test',
+    issuingAgency: 'testCode',
+    legislation: {
+      act: '1',
+      regulation: '2',
+      section: '3',
+      subSection: '4',
+      paragraph: '5',
+      legislationDescription: 'This sounds serious'
+    },
+    offence: 'None',
+    penalties: '',
+    projectName: 'Test Project',
+    location: 'BC',
+    centroid: ['1', '2'],
+    outcomeDescription: 'The outcome was a success, hooray!'
+  }
+];
+const expectedOutput = `Record Type,Issued On,Issued To,Summary,Issuing Agency,Legislation Act,Regulation,Section,Sub Section,Paragraph,Description,Offence,Penalties,Site/Project,Location,Latitude,Longitude,Outcome Description
+"testRecord","2023-10-11","Company",This company doesnt exist,"In summary, this is a test","testCode","1","2","3","4","5","This sounds serious","None","","BC","1","2","The outcome was a success, hooray!"`;
+
+describe('RecordUtils', () => {
+  describe('exportToCsv', () => {
+    it('should call the download function with the expected data', () => {
+      // Defining mocks
+      const urlMock = window.URL.createObjectURL(new Blob([expectedOutput], { type: 'text/plain' }));
+
+      // Defining spies
+      const downloadSpy = jasmine.createSpyObj('a', ['click']);
+      spyOn(moment.prototype, 'format').and.returnValue('this-is-the-time');
+      spyOn(document, 'createElement').and.returnValue(downloadSpy);
+      spyOn(window.URL, 'createObjectURL').and.returnValue(urlMock);
+
+      // Executing export function
+      RecordUtils.exportToCsv(mockData);
+
+      // Ensuring expected output
+      expect(document.createElement).toHaveBeenCalledTimes(1);
+      expect(document.createElement).toHaveBeenCalledWith('a');
+
+      expect(downloadSpy.href).toBe(urlMock);
+      expect(downloadSpy.download).toContain('nrced-export-this-is-the-time.csv');
+    });
+  });
+});
