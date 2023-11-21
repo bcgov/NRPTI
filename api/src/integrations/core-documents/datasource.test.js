0e0ba7bb8e092a5998f0701f0f76b9509779ee20
David <62899351+davidclaveau@users.noreply.github.com>
2023-11-21 11:25:42 -0800

    add additional testing for datasource in core-documents

--- api/src/integrations/core-documents/datasource.test.js
+++ api/src/integrations/core-documents/datasource.test.js
@@ -1,5 +1,17 @@
 const DataSource = require('./datasource');
 const integrationUtils = require('../integration-utils');
+const mongoose = require('mongoose');
+const axios = require('axios');
+const { Readable } = require('stream');
+const DocumentController = require('../../controllers/document-controller');
+const permitUtils = require('./permit-utils');
+
+jest.mock('../../controllers/document-controller');
+jest.mock('axios');
+jest.mock('./permit-utils', () => ({
+  transformRecord: jest.fn(),
+  updateRecord: jest.fn(),
+}));
 
 describe('CoreDocumentsDataSource', () => {
   describe('constructor', () => {
@@ -23,10 +35,51 @@ describe('CoreDocumentsDataSource', () => {
     });
   });
 
+  describe('run', () => {
+    it('executes run method successfully', async () => {
+      const mockedTaskAuditRecord = { updateTaskRecord: jest.fn().mockResolvedValueOnce({ status: 'Running' }) };
+      const dataSource = new DataSource(mockedTaskAuditRecord);
+  
+      await dataSource.run();
+  
+      expect(mockedTaskAuditRecord.updateTaskRecord).toHaveBeenCalledWith({ status: 'Running' });
+    });
+  });
+
   describe('processRecord', () => {
     it('throws error if no permit', () => {
       const dataSource = new DataSource();
       expect(dataSource.processRecord(null, {})).resolves.toEqual(false)
+    });
+  });
+
+  describe('getPermits', () => {
+    it('should call getPermits with mocked data', async () => {
+      const existingRecord = { 
+        _id: 123,
+        _sourceDocumentRefId: 'permit_amendment_guid',
+        documents: []
+      };
+      const mockFind = jest.fn().mockReturnThis();
+      const mockPopulate = jest.fn().mockResolvedValue([existingRecord]);
+
+      const mockPermitAmendment = {
+        find: mockFind,
+        populate: mockPopulate
+      };
+
+      mongoose.model = jest.fn(() => mockPermitAmendment);
+
+      const dataSource = new DataSource();
+      await dataSource.getPermits();
+
+      expect(mongoose.model).toHaveBeenCalledWith('Permit');
+      expect(mockFind).toHaveBeenCalledWith({
+        _schemaName: 'Permit',
+        _sourceDocumentRefId: { $ne: null },
+        documents: []
+      });
+      expect(mockPopulate).toHaveBeenCalledWith('_flavourRecords');
     });
   });
 
@@ -45,6 +98,117 @@ describe('CoreDocumentsDataSource', () => {
       const token = await dataSource.getDownloadToken('testing');
 
       expect(token).toEqual('testing');
+    });
+  });
+
+  describe('getTemporaryDocument', () => {
+    global.CORE_DOC_MANAGER_HOST = 'http://localhost:3000'
+
+    it('should throw an error if documentId or documentName is missing', async () => {
+      const dataSource = new DataSource();
+
+      await expect(dataSource.getTemporaryDocument(null, 'example.docx')).rejects.toThrow(
+        'getTemporaryDocument - param documentId must not be null'
+      );
+
+      await expect(dataSource.getTemporaryDocument('documentId', null)).rejects.toThrow(
+        'getTemporaryDocument - param documentName must not be null'
+      );
+    });
+
+    it('should download and save a document to a temporary location with a mocked URL', async () => {
+      const documentId = 'documentId';
+      const documentName = 'example.docx';
+      const mockDownloadToken = 'downloadToken';
+      const mockResponseStream = new Readable();
+      mockResponseStream.push('file content');
+      mockResponseStream.push(null);
+      const mockResponse = { data: mockResponseStream };
+
+      axios.get.mockResolvedValue(mockResponse);
+
+      const dataSource = new DataSource();
+
+      dataSource.getDownloadToken = jest.fn().mockResolvedValue(mockDownloadToken);
+
+      await dataSource.getTemporaryDocument(documentId, documentName);
+
+      expect(axios.get).toHaveBeenCalledWith(
+        `https://minesdigitalservices.gov.bc.ca/document-manager/documents?token=${mockDownloadToken}`,
+        expect.objectContaining({
+          responseType: 'stream',
+          headers: expect.any(Object),
+        })
+      );
+    });
+  });
+
+  describe('putFileS3', () => {
+    it('should store a document in S3 and return Document ID', async () => {
+      const fileContent = 'file content'; 
+      const fileName = 'example.docx'; 
+
+      const mockDocumentResponse = { docResponse: { _id: 'documentId' } }; 
+
+      DocumentController.createS3Document.mockResolvedValue(mockDocumentResponse);
+
+      const dataSource = new DataSource();
+
+      const result = await dataSource.putFileS3(fileContent, fileName);
+
+      expect(DocumentController.createS3Document).toHaveBeenCalledWith(
+        fileName,
+        fileContent,
+        expect.any(String) 
+      );
+
+      expect(result).toBe('documentId'); 
+    });
+  });
+
+  describe('updatePermit', () => {
+    it('should throw an error when permit is null', async () => {
+      const documentId = 'documentId';
+      const dataSource = new DataSource();
+
+      await expect(dataSource.updatePermit(null, documentId, permitUtils)).rejects.toThrow(
+        'updateAmendment - param permit must not be null and contain documents.'
+      );
+    });
+
+    it('should throw an error when permit is null', async () => {
+      const permit = { documents: [] };
+      const dataSource = new DataSource();
+
+      await expect(dataSource.updatePermit(permit, null, permitUtils)).rejects.toThrow(
+        'updateAmendment - param documentId must not be null.'
+      );
+    });
+
+    it('should successfully call transformRecord and updateRecord', async () => {
+      const permit = { documents: [] };
+      const documentId = 'documentId';
+
+      permitUtils.transformRecord.mockImplementation((permit) => permit);
+      permitUtils.updateRecord.mockResolvedValue([{ status: 'success' }]);
+
+      const dataSource = new DataSource();
+
+      await dataSource.updatePermit(permit, documentId, permitUtils);
+
+      expect(permitUtils.transformRecord).toHaveBeenCalled();
+      expect(permitUtils.updateRecord).toHaveBeenCalled();
+    });
+
+    it('should fail to call transformRecord and updateRecord', async () => {
+      const permit = { documents: [] };
+      const documentId = 'documentId';
+
+      permitUtils.transformRecord.mockImplementation(new Error('Failed to transform record'));
+
+      const dataSource = new DataSource();
+
+      await expect(dataSource.updatePermit(permit, documentId, permitUtils)).rejects.toThrowError();
     });
   });
 });
