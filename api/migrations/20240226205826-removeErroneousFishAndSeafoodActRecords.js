96f788d047812120cfea763569d0def3ea9172d0
David <62899351+davidclaveau@users.noreply.github.com>
2024-02-26 13:26:03 -0800

    add migration to delete all fish and seafood act inspections

---
+++ api/migrations/20240226205826-removeErroneousFishAndSeafoodActRecords.js
@@ -0,0 +1,56 @@
+'use strict';
+
+var dbm;
+var type;
+var seed;
+
+/**
+  * We receive the dbmigrate dependency from dbmigrate initially.
+  * This enables us to not have to rely on NODE_PATH.
+  */
+exports.setup = function(options, seedLink) {
+  dbm = options.dbmigrate;
+  type = dbm.dataType;
+  seed = seedLink;
+};
+
+exports.up = async function(db) {
+  const mClient = await db.connection.connect(db.connectionString, { native_parser: true });
+  const nrpti = mClient.collection('nrpti');
+
+  try {
+    console.log('Deleting all Inspections with Fish and Seafood Act legislation');
+    await nrpti.deleteMany({
+      $or: [
+        {"_schemaName":"Inspection", "legislation": {
+          $elemMatch: {
+            "act": "Fish and Seafood Act"
+          }
+        }},
+        {"_schemaName":"InspectionNRCED", "legislation": {
+          $elemMatch: {
+            "act": "Fish and Seafood Act"
+          }
+        }},
+        {"_schemaName":"InspectionBCMI", "legislation": {
+          $elemMatch: {
+            "act": "Fish and Seafood Act"
+          }
+        }}
+      ]
+    });
+  } catch (err) {
+    console.log(`Error finding records: ${err}`);
+  } finally {
+    console.log('Closing connection');
+    mClient.close();
+  }
+};
+
+exports.down = function(db) {
+  return null;
+};
+
+exports._meta = {
+  "version": 1
+};
