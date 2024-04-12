ea3f5ca94c71833123fc3733862367c997d2bd5b
acatchpole <adamantiumite@gmail.com>
2024-04-12 10:23:22 -0700

    fixed incorrect function description for appendActCodesToActNames

--- angular/projects/admin-nrpti/src/app/records/utils/record-utils.ts
+++ angular/projects/admin-nrpti/src/app/records/utils/record-utils.ts
@@ -277,7 +277,7 @@ export class RecordUtils {
    * Adds the act code to a list of act names for a search query
    * @param {Object} actsSTring - a string of comma-seperated act names.
    * @param {ServiceFactory} factoryService - The service factory used to create data service instances.
-   * @returns {void} Modifies the object in place.
+   * @returns {string} a string with comma-serparated act names followed by comma sperated act codes
    */
   appendActCodesToActNames(actsString, factoryService) {
     const dataservice = new ActDataServiceNRPTI(factoryService);
