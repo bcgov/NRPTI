de9e197f0b1527cf9f56885e375fe92580e88a34
acatchpole <adamantiumite@gmail.com>
2024-06-20 15:17:16 -0700

    removed minePermitNames from individual files as they are now in recordUtils

--- angular/projects/admin-nrpti/src/app/mines/mines-record-add/mines-record-add.component.ts
+++ angular/projects/admin-nrpti/src/app/mines/mines-record-add/mines-record-add.component.ts
@@ -43,7 +43,6 @@ export class MinesRecordAddComponent implements OnInit, OnDestroy {
       return item.displayName;
     })
     .sort();
-  public permitTypes = ['ALG', 'OGP', 'AMD'];
 
   // Documents
   public documents = [];
--- angular/projects/admin-nrpti/src/app/mines/mines-records-edit/mines-records-edit.component.ts
+++ angular/projects/admin-nrpti/src/app/mines/mines-records-edit/mines-records-edit.component.ts
@@ -49,7 +49,6 @@ export class MinesRecordsEditComponent implements OnInit {
       return item.displayName;
     })
     .sort();
-  public permitTypes = ['ALG', 'OGP', 'AMD'];
 
   public recordAgencies = Picklists.collectionAgencyCodePicklist;
 
