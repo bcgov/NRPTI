44eb0b6d6e318e284dbaf796546a208af7883486
Fergus MacConnell WLRS:EX <f.macconnell@gmail.com>
2025-11-25 10:56:03 -0800

    🙃

--- angular/karma.conf.js
+++ angular/karma.conf.js
@@ -38,7 +38,7 @@ module.exports = function (config) {
     colors: true,
     logLevel: config.LOG_INFO,
     autoWatch: true,
-    browsers: process.env.CI === 'true' ? ['ChromeHeadless'] : ['ChromeHeadlessNoSandbox'] ,
+    browsers: process.env.CI === 'true' ? ['ChromeHeadlessNoSandbox'] : ['ChromeHeadless'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
         base: 'ChromeHeadless',
