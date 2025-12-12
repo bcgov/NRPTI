7513891452ce641cdb808a89c9ac5060a5f9870c
Fergus MacConnell <f.macconnell@gmail.com>
2025-12-12 15:38:58 -0800

    [TASK][NRPTI-1416] Add time check (#1434)
    * Add some temporary logs for debugging issue in dev.
    
    * Add more logs.
    
    * Add console time to get search results logic.
    
    * 🙃
    
    * Update logs.
    
    * Add console timeEnd.
    
    * Update logs.
    
    * Update logs.
    
    * Add time end.

--- angular/projects/global/src/lib/components/table-template/table-row.directive.ts
+++ angular/projects/global/src/lib/components/table-template/table-row.directive.ts
@@ -55,12 +55,14 @@ export class TableRowDirective implements OnInit, OnChanges, OnDestroy {
    * @memberof TableRowDirective
    */
   loadComponent() {
+    console.time("TableRowDirective - loadComponent");
     const tableComponentRef: ComponentRef<TableRowComponent> = this.injectComponentService.injectComponentIntoView(
       this.viewContainerRef,
       this.rowObject.component || this.tableData.component
     );
 
     this.setRowComponentData(tableComponentRef.instance);
+    console.timeEnd("TableRowDirective - loadComponent");
   }
 
   /**
