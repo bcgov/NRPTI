3dd15efd5cc54ceae8c25073d9e34f2477f0ce9b
Fergus MacConnell WLRS:EX <f.macconnell@gmail.com>
2025-12-15 10:30:37 -0800

    Clean up imports.

--- angular/projects/global/src/lib/services/search.service.ts
+++ angular/projects/global/src/lib/services/search.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Observable, tap } from 'rxjs';
+import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
 import { SearchResults } from '../models/search';
